b9a2062d285214aee2adef5026f90f7a260cbcbe
Chris Green <greenc@fnal.gov>
2010-09-01 13:52:15 -0500

    Ignore source control directories.

--- ups/update_sources.sh
+++ ups/update_sources.sh
@@ -10,7 +10,8 @@ TOP=${1:-ART}
 # ======================================================================
 # Run scripts to update
 
-for F in `find $TOP/ -type f`; do
+for F in `find $TOP \( -type d \( -name .git -o -name .svn -o -name CVS \) \
+                       -prune \) -o -type f -print`; do
   echo "$F"
   ed "$F" < pop.ed > /dev/null 2>&1
   ed "$F" < movedfile_20100901_112607.ed > /dev/null 2>&1
