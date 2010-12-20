83f27247456dee8c198a9533361db4e4186f45f2
Walter E Brown <wb@fnal.gov>
2010-12-20 15:12:46 -0600

    Remove residue from multiple-plugins-per-file era.

--- art/Framework/Services/Basic/plugins/TFileService.cc
+++
@@ -1,5 +0,0 @@
-#include "art/Framework/Services/Registry/ServiceMaker.h"
-#include "art/Framework/Core/MakerMacros.h"
-#include "art/Framework/Services/Basic/TFileService.h"
-
-DEFINE_FWK_SERVICE( TFileService );
