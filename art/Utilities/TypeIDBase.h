1a7005186add29faef756a3826551db716880702
Walter E Brown <wb@fnal.gov>
2010-12-22 10:28:40 -0600

    Clean up.

--- art/Utilities/TypeIDBase.h
+++ art/Utilities/TypeIDBase.h
@@ -1,80 +1,81 @@
 #ifndef FWCore_Utilities_TypeIDBase_h
 #define FWCore_Utilities_TypeIDBase_h
-// -*- C++ -*-
-//
-// Package:     Utilities
-// Class  :     TypeIDBase
-//
-/**\class TypeIDBase TypeIDBase.h FWCore/Utilities/interface/TypeIDBase.h
 
- Description: Base class for classes used to compare C++ types
-
- Usage:
-    This class is not meant to be used polymorphically (which is why there is no virtual destructor).
- Instead it is used to hold a common methods needed by all type comparing classes.
-
-*/
+// ======================================================================
 //
-// Original Author:  Chris Jones
-//         Created:  Thu Nov 10 14:59:35 EST 2005
+// TypeIDBase - Base class for classes used to compare C++ types
 //
+// Note:
+//   This class is not meant to be used polymorphically (which is why
+// there is no virtual destructor).  Instead it is used to hold a common
+// methods needed by all type comparing classes.
 //
+// ======================================================================
 
-// system include files
 #include <typeinfo>
 
-// user include files
-
-// forward declarations
 namespace art {
-   class TypeIDBase
-   {
-
-    public:
-      struct Def { };
-
-      TypeIDBase() :
-         t_(&(typeid(Def)))
-      { }
-
-      TypeIDBase(const TypeIDBase& other) :
-         t_(other.t_)
-      { }
-
-      explicit TypeIDBase(const std::type_info& t) :
-         t_(&t)
-      { }
-
-      // ---------- const member functions ---------------------
-
-      /** Returned C-style string owned by system; do not delete[] it.
-         This is the (horrible, mangled, platform-dependent) name of
-         the type. */
-      const char* name() const { return t_->name(); }
-
-      bool operator<(const TypeIDBase& b) const { return t_->before(*(b.t_)); }
-      bool operator==(const TypeIDBase& b) const { return (*t_) == *(b.t_); }
-
-    protected:
-      const std::type_info& typeInfo() const {return *t_;}
-
-    private:
-
-      //const TypeIDBase& operator=(const TypeIDBase&); // stop default
-
-      // ---------- member data --------------------------------
-      //NOTE: since the compiler generates the type_info's and they have a lifetime
-      //  good for the entire application, we do not have to delete it
-      //  We also are using a pointer rather than a reference so that operator= will work
-      const std::type_info* t_;
-   };
-
-   inline bool operator>(const TypeIDBase& a, const TypeIDBase& b)
-   { return b<a; }
-
-   inline bool operator!=(const TypeIDBase& a, const TypeIDBase& b)
-   { return !(a==b); }
-
+  class TypeIDBase;
+  bool
+    operator > ( TypeIDBase const & a, TypeIDBase const & b );
+  bool
+    operator != ( TypeIDBase const & a, TypeIDBase const & b );
 }
 
-#endif
+// ----------------------------------------------------------------------
+
+class art::TypeIDBase
+{
+public:
+  struct Def { };
+
+  // c'tors:
+  TypeIDBase( )
+  : t_( & typeid(Def) )
+  { }
+  explicit TypeIDBase( std::type_info const & t)
+  : t_( &t )
+  { }
+
+  // use compiler-generated copy c'tor, copy assignment, and d'tor
+
+  // Returned C-style string owned by system; do not delete[] it.
+  // This is the (horrible, mangled, platform-dependent) name of the type.
+  char const *
+    name( ) const
+  { return t_->name(); }
+
+  // comparators:
+  bool
+    operator < ( const TypeIDBase & other ) const
+  { return t_->before(*other.t_); }
+  bool
+    operator == ( const TypeIDBase & other ) const
+  { return *t_ == *other.t_; }
+
+protected:
+  std::type_info const &
+    typeInfo( ) const { return *t_; }
+
+private:
+  // NOTE: since (a) the compiler generates the type_info's, and
+  // (b) they have a lifetime good for the entire application,
+  // we do not have to delete it.
+  // We use a pointer rather than a reference so that assignment will work
+  const std::type_info * t_;
+
+};  // TypeIDBase
+
+// ----------------------------------------------------------------------
+
+inline bool
+  art::operator > ( TypeIDBase const & a, TypeIDBase const & b )
+{ return b < a; }
+
+inline bool
+  art::operator != ( TypeIDBase const & a, TypeIDBase const & b )
+{ return ! (a==b); }
+
+// ======================================================================
+
+#endif  // FWCore_Utilities_TypeIDBase_h
