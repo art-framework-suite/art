2703985b71ed8f6e906ec3dd298a8b499fec9817
Walter E Brown <wb@fnal.gov>
2010-12-22 11:34:24 -0600

    Remove VectorInputSourceMacros.

--- art/Framework/IO/Input/PoolRASource_plugin.cc
+++ art/Framework/IO/Input/PoolRASource_plugin.cc
@@ -1,7 +1,5 @@
 #include "art/Framework/Core/InputSourceMacros.h"
 #include "art/Framework/IO/Input/PoolSource.h"
-#include "art/Framework/IO/Sources/VectorInputSourceMacros.h"
 
 using art::PoolRASource;
 DEFINE_FWK_INPUT_SOURCE(PoolRASource);
-DEFINE_FWK_VECTOR_INPUT_SOURCE(PoolRASource);
--- art/Framework/IO/Input/PoolSource_plugin.cc
+++ art/Framework/IO/Input/PoolSource_plugin.cc
@@ -1,7 +1,5 @@
 #include "art/Framework/Core/InputSourceMacros.h"
 #include "art/Framework/IO/Input/PoolSource.h"
-#include "art/Framework/IO/Sources/VectorInputSourceMacros.h"
 
 using art::PoolSource;
 DEFINE_FWK_INPUT_SOURCE(PoolSource);
-DEFINE_FWK_VECTOR_INPUT_SOURCE(PoolSource);
