4d143ec5b2abcea9f62e0e7b958e11cf705f4cf1
Walter E Brown <wb@fnal.gov>
2011-01-03 14:25:53 -0600

    Redesign and update to use new LibraryManager.

--- art/Framework/Core/Factory.cc
+++ art/Framework/Core/Factory.cc
@@ -1,64 +1,67 @@
+// ======================================================================
+//
+// Factory
+//
+// ======================================================================
+
 #include "art/Framework/Core/Factory.h"
 
-#include "art/Utilities/DebugMacros.h"
 #include "art/Utilities/Exception.h"
-#include "cetlib/container_algorithms.h"
-#include <iostream>
+#include <utility>
 
+using namespace art;
 
-namespace
+// ----------------------------------------------------------------------
+
+Factory::Factory()
+: mgr_map_( )
+{ }
+
+Factory::~Factory()
+{ }
+
+// ----------------------------------------------------------------------
+
+Factory &
+  Factory::the_factory_()
 {
-  std::string translate_typespec_to_libname(std::string const& str)
-
-  {
-    // TODO: consider using std::tranlate and a lambda function ...
-    std::string result(str);
-
-    for (std::string::iterator i = result.begin(), e = result.end();
-         i != e; ++i)
-      {
-        if (*i == '/') *i = '_';
-      }
-  }
+  static  Factory  the_factory;
+  return the_factory;
 }
 
-namespace art {
+// ----------------------------------------------------------------------
 
-  Factory::~Factory()
-  {
+std::auto_ptr<Worker>
+  Factory::makeWorker( std::string       const & kind
+                     , WorkerParams      const & p
+                     , ModuleDescription const & md
+                     )
+{
+  std::shared_ptr<LibraryManager> libmgr_p;
+  mgr_map_t & mm = Factory::the_factory_().mgr_map_;
+  mgr_map_t::iterator it = mm.find(kind);
+  if( it == mm.end() ) {
+    libmgr_p.reset( new LibraryManager(kind) );
+    mm.insert( std::make_pair(kind, libmgr_p) );
   }
+  else
+    libmgr_p = it->second;
 
-  Factory&
-  Factory::the_instance_()
-  {
-    static Factory me;
-    return me;
-  }
+  std::string libspec( p.pset_->get<std::string>("_module_type") );
+  typedef Worker*  (*make_t)( WorkerParams      const &
+                            , ModuleDescription const &
+                            );
+  make_t * symbol
+    = static_cast<make_t*>( libmgr_p->getSymbolByLibspec(libspec, "make_temp") );
+  if( symbol == 0 )
+    throw art::Exception(errors::Configuration,"UnknownModule")
+      << "Module " << libspec
+      << " with version " << p.releaseVersion_
+      << " was not registered.\n"
+      << "Perhaps your module type is misspelled or is not a framework plugin.";
 
-  Factory::Factory() :
-    libman_("plugin")
-  {
-  }
+  return std::auto_ptr<Worker>( (*symbol)(p,md) );
 
-  std::auto_ptr<Worker> Factory::makeWorker(WorkerParams const& p,
-                                            ModuleDescription const& md)
-  {
-    typedef Worker* (*factor_fcn_t)(WorkerParams const&, ModuleDescription const&);
-    Factory& me = the_instance_();
+}  // makeWorker()
 
-    std::string modtype(p.pset_->get<std::string>("_module_type"));
-    std::string libname = translate_typespec_to_libname(modtype);
-    factory_fcn_t* symbol = (factory_fcn_t*)libman_.getSymbol(libname, "make_temp");
-    if (symbol == 0)
-      throw art::Exception(errors::Configuration,"UnknownModule")
-        << "Module " << modtype
-        << " with version " << p.releaseVersion_
-        << " was not registered.\n"
-        << "Perhaps your module type is misspelled or is not a "
-        << "framework plugin.";
-
-    return (*symbol)(p,md);
-
-  }
-
-}  // art
+// ======================================================================
--- art/Framework/Core/Factory.h
+++ art/Framework/Core/Factory.h
@@ -1,30 +1,54 @@
 #ifndef FWCore_Framework_Factory_h
 #define FWCore_Framework_Factory_h
 
+// ======================================================================
+//
+// Factory
+//
+// ======================================================================
+
 #include "art/Framework/Core/LibraryManager.h"
 #include "art/Framework/Core/Worker.h"
 #include "art/Framework/Core/WorkerParams.h"
-#include <memory>
+#include "art/Persistency/Provenance/ModuleDescription.h"
+#include "cpp0x/memory"
+#include <map>
+#include <string>
 
 namespace art {
+  class Factory;
+}
 
-  class Factory
-  {
-  public:
-    // We are not caching the discovered symbols. If we did, we'd use
-    // a map of typespec (string) to maker-function-pointer
+// ----------------------------------------------------------------------
 
-    static
-    std::auto_ptr<Worker> makeWorker(WorkerParams const wp&,
-                                     ModuleDescription const& md);
+class art::Factory
+{
+  // non-copyable:
+  Factory( Factory const & );
+  void  operator = ( Factory const & );
 
-  private:
-    static Factory& the_instance_();
-    Factory();
-    ~Factory();
-    LibraryManager libman;
-  };  // Factory
+public:
+  static std::auto_ptr<Worker>
+    makeWorker( std::string       const & kind
+              , WorkerParams      const & wp
+              , ModuleDescription const & md
+              );
 
-}  // art
+private:
+  typedef  std::map< std::string
+                   , std::shared_ptr<LibraryManager>
+                   >
+           mgr_map_t;
+  mgr_map_t  mgr_map_;
+
+  Factory();
+  ~Factory();
+
+  static Factory &
+    the_factory_();
+
+};  // Factory
+
+// ======================================================================
 
 #endif
