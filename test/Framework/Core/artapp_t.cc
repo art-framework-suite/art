5127a068d91ed7f97e325c0dba82fa9072febce7
Chris Green <greenc@fnal.gov>
2011-01-07 12:00:19 -0600

    Re-work artapp_t.cc as a BOOST unit test including simple startup and error handling checks.

--- test/Framework/Core/artapp_t.cc
+++ test/Framework/Core/artapp_t.cc
@@ -1,9 +1,30 @@
 extern "C" { int artapp(int argc, char* argv[]); }
 
-#include <assert.h>
+#define BOOST_TEST_MODULE ( artapp test )
+#include "boost/test/auto_unit_test.hpp"
 
-int main(int argc, char* argv[])
-{
-  int rc = artapp(argc, argv);
-  assert (rc == 0);
+#include "cetlib/exception.h"
+
+BOOST_AUTO_TEST_SUITE ( artappTests )
+
+BOOST_AUTO_TEST_CASE ( NoConfig ) {
+   char const* strings[] = { "artapp_t" };
+   BOOST_CHECK( artapp(1, const_cast<char**>(strings) ) == 7001 );
 }
+
+BOOST_AUTO_TEST_CASE ( testHelp ) {
+   char const* strings[] = { "artapp_t", "--help" };
+   BOOST_CHECK( artapp(2, const_cast<char**>(strings) ) == 0 );
+}
+
+BOOST_AUTO_TEST_CASE ( testBadConfigOption ) {
+   char const* strings[] = { "artapp_t", "--config" };
+   BOOST_CHECK( artapp(2, const_cast<char**>(strings) ) == 7000 );
+}
+
+BOOST_AUTO_TEST_CASE ( testEmptyConfig ) {
+   char const* strings[] = { "artapp_t", "--config", "empty_config.fcl" };
+   BOOST_CHECK( artapp(3, const_cast<char**>(strings) ) == 0 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
