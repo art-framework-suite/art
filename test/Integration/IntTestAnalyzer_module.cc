9f694b84842d4562083ce89082035b67ccc71ac4
Marc Paterno <paterno@fnal.gov>
2011-01-14 14:11:55 -0600

    Enhance configurability of IntTestAnalyzer

--- test/Integration/IntTestAnalyzer_module.cc
+++ test/Integration/IntTestAnalyzer_module.cc
@@ -25,13 +25,22 @@ class arttest::IntTestAnalyzer
 public:
   IntTestAnalyzer(const fhicl::ParameterSet& conf) :
     value_(conf.get<int>("valueMustMatch")),
-    moduleLabel_(conf.get<std::string>("input_label"))
+    moduleLabel_(conf.get<std::string>("input_label")),
+    require_presence_(conf.get<bool>("require_presence", true))    
   { }
 
   void analyze(const art::Event& e)
   {
+    if (require_presence_)
+      require_product_presence(e);
+    else
+      require_product_absence(e);
+  }
+
+  void require_product_presence(art::Event const& e)
+  {
     art::Handle<IntProduct> handle;
-    e.getByLabel(moduleLabel_,handle);
+    e.getByLabel(moduleLabel_, handle);
     if(handle->value != value_) {
       throw cet::exception("ValueMismatch")
         << "The value for \"" << moduleLabel_
@@ -41,9 +50,29 @@ public:
     }
   }
 
+  void require_product_absence(art::Event const& e)
+  {
+    art::Handle<IntProduct> handle;
+    try
+      {
+	e.getByLabel(moduleLabel_, handle);
+	throw cet::exception("WronglyFound")
+	  << "The product for \"" << moduleLabel_
+	  << "\" is supposed to be absent, but is present\n";
+      }
+    catch (art::Exception& ex)
+      {
+	assert(ex.categoryCode() == art::errors::ProductNotFound);
+      }
+    // Other exception types will propagate out, and
+    // indicate failure...    
+  }
+
+
 private:
   int value_;
   std::string moduleLabel_;
+  bool require_presence_;
 };  // IntTestAnalyzer
 
 DEFINE_ART_MODULE(arttest::IntTestAnalyzer);
