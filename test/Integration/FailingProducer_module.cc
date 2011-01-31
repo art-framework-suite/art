8858e1714a7fe730701504cc3f4a4bacde67f01d
Chris Green <greenc@fnal.gov>
2011-01-31 10:06:02 -0600

    Resurrect FailingProducer

---
+++ test/Integration/FailingProducer_module.cc
@@ -0,0 +1,30 @@
+#include "art/Framework/Core/EDProducer.h"
+#include "test/TestObjects/ToyProducts.h"
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace arttest {
+   class FailingProducer;
+}
+
+//--------------------------------------------------------------------
+//
+// throws an exception.
+// Announces an IntProduct but does not produce one;
+// every call to FailingProducer::produce throws a cms exception
+//
+class arttest::FailingProducer : public art::EDProducer {
+public:
+   explicit FailingProducer(fhicl::ParameterSet const&) {
+      produces<arttest::IntProduct>();
+   }
+   virtual ~FailingProducer() { }
+   virtual void produce(art::Event& e);
+};
+
+void
+arttest::FailingProducer::produce(art::Event&) {
+   // We throw an edm exception with a configurable action.
+   throw art::Exception(art::errors::ProductNotFound) << "Intentional 'ProductNotFound' exception for testing purposes\n";
+}
+
+DEFINE_ART_MODULE(arttest::FailingProducer);
