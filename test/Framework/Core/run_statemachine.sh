3de61eed9e964966ad49f1b142dd806898f2f4b3
Marc Paterno <paterno@fnal.gov>
2011-02-13 12:56:27 -0600

    Remove needless verbosity in test names

--- test/Framework/Core/run_statemachine.sh
+++ test/Framework/Core/run_statemachine.sh
@@ -7,7 +7,7 @@
 #LOCAL_TMP_DIR=/uscms_data/d1/wdd/CMSSW_1_8_0_pre2/tmp/slc4_ia32_gcc345
 #LOCAL_TEST_DIR=/uscms_data/d1/wdd/CMSSW_1_8_0_pre2/src/FWCore/Framework/test
 
-exe=${LOCAL_TMP_DIR}/src/FWCore/Framework/test/TestFWCoreFrameworkStatemachine/TestFWCoreFrameworkStatemachine
+exe=${LOCAL_TMP_DIR}/src/FWCore/Framework/test/TestFrameworkCoreStatemachine/TestFrameworkCoreStatemachine
 input=${FW_HOME}/FWCore/Framework/test/unit_test_outputs/statemachine_
 output=statemachine_output_
 reference_output=${FW_HOME}/FWCore/Framework/test/unit_test_outputs/statemachine_output_
@@ -19,7 +19,7 @@ pushd ${LOCAL_TMP_DIR}
   for i in 1 2 5 6 7 8 9 10 11
   do
 
-    ${exe} -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFWCoreFrameworkStatemachine with input ${i}" $?
+    ${exe} -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFrameworkCoreStatemachine with input ${i}" $?
     diff ${reference_output}${i}.txt ${output}${i}.txt || die "comparing ${output}${i}.txt" $?
 
   done
@@ -27,7 +27,7 @@ pushd ${LOCAL_TMP_DIR}
   for i in 3 4
   do
 
-    ${exe} -s -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFWCoreFrameworkStatemachine with input ${i}" $?
+    ${exe} -s -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFrameworkCoreStatemachine with input ${i}" $?
     diff ${reference_output}${i}.txt ${output}${i}.txt || die "comparing ${output}${i}.txt" $?
 
   done
@@ -35,7 +35,7 @@ pushd ${LOCAL_TMP_DIR}
   for i in 12
   do
 
-    ${exe} -m -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFWCoreFrameworkStatemachine with input ${i}" $?
+    ${exe} -m -i ${input}${i}.txt -o ${output}${i}.txt || die "TestFrameworkCoreStatemachine with input ${i}" $?
     diff ${reference_output}${i}.txt ${output}${i}.txt || die "comparing ${output}${i}.txt" $?
 
   done
