b5bd26b0d740fe8c1647abdef896b5f6f64d365d
Chris Green <greenc@fnal.gov>
2011-02-18 14:06:02 -0600

    Fix faulty test script.

--- test/Integration/EventSelectorFromFile_t.sh
+++ test/Integration/EventSelectorFromFile_t.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. cet_test_functions.sh -r
+. cet_test_functions.sh
 
 rm -f out1.root out2.root cerr.log warnings.log
 
 run_art EventSelectorFromFile_w.fcl
 
-check_files "out.root" "out2.root"
+check_files "out1.root" "out2.root"
 
 run_art EventSelectorFromFile_r1.fcl
 
