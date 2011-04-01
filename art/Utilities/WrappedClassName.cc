d05fa12d339fc05263e1e96c152fa25aa84f58ae
Chris Green <greenc@fnal.gov>
2011-03-31 20:05:44 -0500

    function-scope consts should be static.

--- art/Utilities/WrappedClassName.cc
+++ art/Utilities/WrappedClassName.cc
@@ -3,9 +3,9 @@
 namespace art {
   std::string
   wrappedClassName(std::string const& className) {
-    std::string const wrapperBegin("art::Wrapper<");
-    std::string const wrapperEnd1(">");
-    std::string const wrapperEnd2(" >");
+    static std::string const wrapperBegin("art::Wrapper<");
+    static std::string const wrapperEnd1(">");
+    static std::string const wrapperEnd2(" >");
     std::string const& wrapperEnd = (className[className.size()-1] == '>' ? wrapperEnd2 : wrapperEnd1);
     std::string wrapped;
     wrapped.reserve(wrapperBegin.size() + className.size() + wrapperEnd.size());
