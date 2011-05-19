da8ae8170e6daf29ca2b985a2648fe54d69b48be
Chris Green <greenc@fnal.gov>
2011-05-19 10:04:32 -0500

    Migration improvements.

--- tools/migration/fix-services.pl
+++ tools/migration/fix-services.pl
@@ -14,6 +14,8 @@ foreach my $service (@system) {
   s&^(\s*#include\s+["<]art/Framework/Services/)Basic(/\Q${service}.h\E[">].*)$&${1}System${2}& and last;
 }
 
+s&^(\s*#include\s+["<]art/Framework/)Core/(RandomNumberGenerator)Service\.h&${1}Services/Optional/${2}.h&;
+
 ### Local Variables:
 ### mode: cperl
 ### End:
