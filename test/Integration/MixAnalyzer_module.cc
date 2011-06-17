ff065b62087a2b219865ba85bb3a898c9a575e62
Chris Green <greenc@fnal.gov>
2011-06-17 13:22:44 -0500

    Update MixAnalyzer_module.cc to use BOOST unit tests and fix minor problem with currently-disabled code.

--- test/Integration/MixAnalyzer_module.cc
+++ test/Integration/MixAnalyzer_module.cc
@@ -7,6 +7,9 @@
 // from art v0_06_02.
 ////////////////////////////////////////////////////////////////////////
 
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/included/unit_test.hpp>
+
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/IO/ProductMix/MixContainerTypes.h"
 #include "art/Persistency/Common/Handle.h"
@@ -57,31 +60,31 @@ void arttest::MixAnalyzer::analyze(art::Event const &e) {
 
   // Double
   art::Handle<double> dH;
-  assert(e.getByLabel(mixFilterLabel_, "doubleLabel", dH));
+  e.getByLabel(mixFilterLabel_, "doubleLabel", dH);
   double dExpected = ((2 * eventCounter_ - 1) * nSecondaries_ + 1) * nSecondaries_ / 2;
-  assert(*dH ==  dExpected);
+  assert(*dH == dExpected);
 
   // IntProduct
   art::Handle<IntProduct> ipH;
-  assert(e.getByLabel(mixFilterLabel_, "IntProductLabel", ipH));
-  assert(ipH->value == dExpected + 1000000 * nSecondaries_); 
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "IntProductLabel", ipH));
+  BOOST_REQUIRE_EQUAL(ipH->value, dExpected + 1000000 * nSecondaries_); 
 
   // String
   art::Handle<std::string> sH;
-  assert(e.getByLabel(mixFilterLabel_, "stringLabel", sH));
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "stringLabel", sH));
   std::ostringstream sExp;
   for (size_t i = 1; i <= nSecondaries_; ++i) {
     sExp << "string value: " << std::setfill('0') << std::setw(7) << (eventCounter_ - 1) * nSecondaries_ + i << "\n";
   }
-  assert(*sH == sExp.str());
+  BOOST_REQUIRE_EQUAL(*sH, sExp.str());
 
   // 1. std::vector<double>
   art::Handle<std::vector<double> > vdH;
-  assert(e.getByLabel(mixFilterLabel_, "doubleCollectionLabel", vdH));
-  assert(vdH->size() == 10 * nSecondaries_);
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "doubleCollectionLabel", vdH));
+  BOOST_REQUIRE_EQUAL(vdH->size(), 10 * nSecondaries_);
   for (size_t i = 0; i < nSecondaries_; ++i) {
     for (size_t j = 1; j < 11; ++j) {
-      assert((*vdH)[i*10 + j - 1] == j + 10 * (i + (eventCounter_ - 1) * nSecondaries_));
+      BOOST_REQUIRE_EQUAL((*vdH)[i*10 + j - 1], j + 10 * (i + (eventCounter_ - 1) * nSecondaries_));
     }
   }
 
@@ -89,37 +92,37 @@ void arttest::MixAnalyzer::analyze(art::Event const &e) {
   // 3. art::PtrVector<double>
   // 4. ProductWithPtrs
   art::Handle<std::vector<art::Ptr<double> > > vpdH;
-  assert(e.getByLabel(mixFilterLabel_, "doubleVectorPtrLabel", vpdH)); // 2.
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "doubleVectorPtrLabel", vpdH)); // 2.
 #ifndef ART_NO_MIX_PTRVECTOR
   art::Handle<art::PtrVector<double> > pvdH;
-  assert(e.getByLabel(mixFilterLabel_, "doubleVectorPtrLabel", vpdH)); // 3.
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "doublePtrVectorLabel", pvdH)); // 3.
 #endif
   art::Handle<arttest::ProductWithPtrs> pwpH;
-  assert(e.getByLabel(mixFilterLabel_, "ProductWithPtrsLabel", pwpH)); // 4.
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "ProductWithPtrsLabel", pwpH)); // 4.
 
   for (size_t i = 0; i < nSecondaries_; ++i) {
-    assert(*(*vpdH)[i * 3 + 0] == (*vdH)[(i * 10) + 0]); // 2.
-    assert(*(*vpdH)[i * 3 + 1] == (*vdH)[(i * 10) + 4]); // 2.
-    assert(*(*vpdH)[i * 3 + 2] == (*vdH)[(i * 10) + 8]); // 2.
+    BOOST_REQUIRE_EQUAL(*(*vpdH)[i * 3 + 0], (*vdH)[(i * 10) + 0]); // 2.
+    BOOST_REQUIRE_EQUAL(*(*vpdH)[i * 3 + 1], (*vdH)[(i * 10) + 4]); // 2.
+    BOOST_REQUIRE_EQUAL(*(*vpdH)[i * 3 + 2], (*vdH)[(i * 10) + 8]); // 2.
 #ifndef ART_NO_MIX_PTRVECTOR
-    assert(*(*pvdH)[i * 3 + 0] == (*vdH)[(i * 10) + 1]); // 3.
-    assert(*(*pvdH)[i * 3 + 1] == (*vdH)[(i * 10) + 5]); // 3.
-    assert(*(*pvdH)[i * 3 + 2] == (*vdH)[(i * 10) + 9]); // 3.
+    BOOST_REQUIRE_EQUAL(*(*pvdH)[i * 3 + 0], (*vdH)[(i * 10) + 1]); // 3.
+    BOOST_REQUIRE_EQUAL(*(*pvdH)[i * 3 + 1], (*vdH)[(i * 10) + 5]); // 3.
+    BOOST_REQUIRE_EQUAL(*(*pvdH)[i * 3 + 2], (*vdH)[(i * 10) + 9]); // 3.
 #endif
-    assert(*(pwpH->vectorPtrDouble())[i * 3 + 0] == *(*vpdH)[i * 3 + 0]); // 4.
-    assert(*(pwpH->vectorPtrDouble())[i * 3 + 1] == *(*vpdH)[i * 3 + 1]); // 4.
-    assert(*(pwpH->vectorPtrDouble())[i * 3 + 2] == *(*vpdH)[i * 3 + 2]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->vectorPtrDouble())[i * 3 + 0], *(*vpdH)[i * 3 + 0]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->vectorPtrDouble())[i * 3 + 1], *(*vpdH)[i * 3 + 1]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->vectorPtrDouble())[i * 3 + 2], *(*vpdH)[i * 3 + 2]); // 4.
 #ifndef ART_NO_MIX_PTRVECTOR
-    assert(*(pwpH->ptrVectorDouble())[i * 3 + 0] == *(*pvdH)[i * 3 + 0]); // 4.
-    assert(*(pwpH->ptrVectorDouble())[i * 3 + 1] == *(*pvdH)[i * 3 + 1]); // 4.
-    assert(*(pwpH->ptrVectorDouble())[i * 3 + 2] == *(*pvdH)[i * 3 + 2]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->ptrVectorDouble())[i * 3 + 0], *(*pvdH)[i * 3 + 0]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->ptrVectorDouble())[i * 3 + 1], *(*pvdH)[i * 3 + 1]); // 4.
+    BOOST_REQUIRE_EQUAL(*(pwpH->ptrVectorDouble())[i * 3 + 2], *(*pvdH)[i * 3 + 2]); // 4.
 #endif
   }
 
   // map_vector<unsigned int>
   art::Handle<cet::map_vector<unsigned int> > mv;
-  assert(e.getByLabel(mixFilterLabel_, "", mv));
-  assert(mv->size() == 5 * nSecondaries_);
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, "", mv));
+  BOOST_REQUIRE_EQUAL(mv->size(), 5 * nSecondaries_);
   cet::map_vector<unsigned int>::const_iterator it = mv->begin();
   size_t delta = 0;
   size_t index = 0;
@@ -127,20 +130,20 @@ void arttest::MixAnalyzer::analyze(art::Event const &e) {
     for (size_t j = 0; j < 5; ++j, ++it) {
       index = 1 + j * 2 + delta + 10 * (i + nSecondaries_ * (eventCounter_ - 1));
       size_t answer = (eventCounter_ - 1) * nSecondaries_ + i + 1;
-      assert(*mv->getOrNull(cet::map_vector_key(static_cast<unsigned int>(index))) ==
+      BOOST_REQUIRE_EQUAL(*mv->getOrNull(cet::map_vector_key(static_cast<unsigned int>(index))),
              answer);
     }
   }
 
   // Bookkeeping.
   art::Handle<std::string> bsH;
-  assert(e.getByLabel(mixFilterLabel_, bsH));
-  assert((*bsH) == "BlahBlahBlah");
+  BOOST_REQUIRE(e.getByLabel(mixFilterLabel_, bsH));
+  BOOST_REQUIRE_EQUAL((*bsH), "BlahBlahBlah");
 
   art::Handle<art::EventIDSequence> eidsH;
-  assert(e.getByLabel(mixFilterLabel_, eidsH));
+  BOOST_REQUIRE((e.getByLabel(mixFilterLabel_, eidsH)));
   for (size_t i = 0; i < nSecondaries_; ++i) {
-    assert((*eidsH)[i].event() == (eventCounter_ - 1) * nSecondaries_ + i + 1);
+    BOOST_REQUIRE_EQUAL((*eidsH)[i].event(), (eventCounter_ - 1) * nSecondaries_ + i + 1);
   }
 }
 
