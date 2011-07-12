83a80cc57ba0c7b61a5147a515fe87f47a559978
Chris Green <artists@fnal.gov>
2011-07-12 16:27:01 -0500

    Update file format version for PtrVector changes; also commments.

--- art/Framework/IO/Root/GetFileFormatEra.cc
+++ art/Framework/IO/Root/GetFileFormatEra.cc
@@ -1,6 +1,25 @@
 // change
 #include "art/Framework/IO/Root/GetFileFormatEra.h"
 
+////////////////////////////////////////////////////////////////////////
+// Era Histoy
+////////////////////////////////////
+//
+// In general:
+//
+// * A change to the persistent format which is both backward- and
+// forawrd-compatible requires no change to the version or era.
+//
+// * A backward-compatible change should change the version.
+//
+// * A non-backward-compatible change should change the era,
+//
+// Era            Comments
+//
+//     -          As forked from CMS.
+// ART_2011_a     Initial ART-only format.
+//
+////////////////////////////////////////////////////////////////////////
 namespace art
 {
    std::string const &getFileFormatEra() {
--- art/Framework/IO/Root/GetFileFormatVersion.cc
+++ art/Framework/IO/Root/GetFileFormatVersion.cc
@@ -5,6 +5,15 @@
 // Version history
 ////////////////////////////////////
 //
+// In general:
+//
+// * A change to the persistent format which is both backward- and
+// forawrd-compatible requires no change to the version or era.
+//
+// * A backward-compatible change should change the version.
+//
+// * A non-backward-compatible change should change the era,
+//
 // Era / Version   Comments
 //
 //    -       11   As forked from CMS.
@@ -12,10 +21,11 @@
 // ART_2011a   2   No FileID in ROOT file.
 // ART_2011a   3   Bunch crossing, orbit number and store number removed
 //                 from EventAuxiliary.
+// ART_2011a   4   PtrVector member indicies_-> indices_.
 //
 ////////////////////////////////////////////////////////////////////////
 
 namespace art
 {
-  int getFileFormatVersion() { return 3; }
+  int getFileFormatVersion() { return 4; }
 }
