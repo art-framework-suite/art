e9acfc52a48334ca703dd8ff30d0b4033a920e99
Chris Green <artists@fnal.gov>
2011-07-13 12:58:03 -0500

    Update test for incremented file version.

--- test/Framework/IO/Root/test_GetFileFormatVersion.cpp
+++ test/Framework/IO/Root/test_GetFileFormatVersion.cpp
@@ -3,6 +3,6 @@
 #include <iostream>
 int main(int argc, char* argv[])
 {
-  assert(art::getFileFormatVersion() == 3);
+  assert(art::getFileFormatVersion() == 4);
   std::cout << "Running " << argv[0] << std::endl;
 }
