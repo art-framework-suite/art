9a2a17fc62e8d166f68cc2767e86633a03734720
Chris Green <artists@fnal.gov>
2011-09-01 13:37:33 -0500

    Facility to compare two pointers for identity.
    Minor formatting change.
    
    Add test for pointersEqual.
    
    According to a further reading on multiple inheritance and thunking, this is the minimal implementation (tested).

---
+++ art/Utilities/pointersEqual.h
@@ -0,0 +1,33 @@
+#ifndef art_Utilities_pointersEqual_h
+#define art_Utilities_pointersEqual_h
+// Compare two pointers taking into account possible multiple inheritance
+
+#include "art/Utilities/Exception.h"
+
+#include "cpp0x/type_traits"
+
+namespace art {
+  template <typename T1, typename T2>
+  inline
+  typename std::enable_if < std::is_same<T1, T2>::value || std::is_base_of<T1, T2>::value || std::is_base_of<T2, T1>::value, bool >::type
+  pointersEqual(T1 * t1, T2 * t2) { return t1 == t2; }
+
+  // Not compatible.
+  template <typename T1, typename T2>
+  typename std::enable_if < !std::is_same<T1, T2>::value && !std::is_base_of<T1, T2>::value && !std::is_base_of<T2, T1>::value, bool >::type
+  pointersEqual(T1 * t1, T2 * t2);
+}
+
+template <typename T1, typename T2>
+typename std::enable_if < !std::is_same<T1, T2>::value && !std::is_base_of<T1, T2>::value && !std::is_base_of<T2, T1>::value, bool >::type
+art::pointersEqual(T1 * t1, T2 * t2)
+{
+  throw art::Exception(art::errors::LogicError)
+      << "Tried to compare two incompatible pointers.\n";
+}
+
+#endif /* art_Utilities_pointersEqual_h */
+
+// Local Variables:
+// mode: c++
+// End:
---
+++ test/Utilities/pointersEqual_t.cc
@@ -0,0 +1,56 @@
+// Test of pointersEqual
+#define BOOST_TEST_MODULE (pointersEqual_t)
+#include "boost/test/auto_unit_test.hpp"
+
+#include "art/Utilities/pointersEqual.h"
+BOOST_AUTO_TEST_SUITE(pointersEqual_t)
+
+class A { };
+class B { };
+class C : public A, public B { };
+struct D : public C { };
+class E : public A { };
+
+BOOST_AUTO_TEST_CASE(basic_types)
+{
+  int i = 3;
+  int j = 3;
+  int * ip1 = &i;
+  int * ip2 = &i;
+  double p = i;
+  BOOST_CHECK(art::pointersEqual(ip1, ip2));
+  BOOST_CHECK(art::pointersEqual(&i, ip1));
+  BOOST_CHECK(art::pointersEqual(&i, ip2));
+  BOOST_CHECK_THROW(art::pointersEqual(&i, &p), art::Exception);
+  BOOST_CHECK(!art::pointersEqual(&i, &j));
+}
+
+BOOST_AUTO_TEST_CASE(inheritance)
+{
+  D d1;
+  D * pd1d1(&d1);
+  D * pd1d2(&d1);
+  A * pd1a1(&d1);
+  B * pd1b1(&d1);
+  D d2;
+  A a1;
+  B b1;
+  E e1;
+  BOOST_CHECK(art::pointersEqual(pd1d1, pd1d2));
+  BOOST_CHECK(art::pointersEqual(pd1d1, pd1a1));
+  BOOST_CHECK(art::pointersEqual(pd1d1, pd1b1));
+  BOOST_CHECK_THROW(art::pointersEqual(pd1a1, pd1b1), art::Exception);
+  BOOST_CHECK_THROW(art::pointersEqual(&a1, &b1), art::Exception);
+  BOOST_CHECK(!art::pointersEqual(pd1a1, &a1));
+  BOOST_CHECK(!art::pointersEqual(pd1a1, &e1));
+  BOOST_CHECK(!art::pointersEqual(&d1, &d2));
+}
+
+BOOST_AUTO_TEST_CASE(constness)
+{
+  D d1;
+  A const * da1(&d1);
+  BOOST_CHECK(art::pointersEqual(&d1, da1));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
