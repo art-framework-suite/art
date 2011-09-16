f887759ab14a7a1feb4bb8a8563adb5448e2ae32
Chris Green <greenc@fnal.gov>
2011-09-16 15:26:04 -0500

    Change our version of the RootClassMapping test to use BOOST_UNIT and check the output internally for the correct streamer invocation.

--- test/Persistency/Provenance/RootClassMapping_t.h
+++ test/Persistency/Provenance/RootClassMapping_t.h
@@ -1,7 +1,7 @@
 #ifndef test_Persistency_Provenance_RootClassMapping_t_h
 #define test_Persistency_Provenance_RootClassMapping_t_h
 
-#include <iostream>
+#include <ostream>
 
 #include "TBuffer.h"
 #include "TClass.h"
@@ -14,38 +14,38 @@ class TestProdStreamer;
 template <typename A, typename B>
 class TestProd {
 public:
-  TestProd()
-    :
-    data() {
-    static TClassRef cl(TClass::GetClass(typeid(TestProd<A, B>)));
-    if (cl->GetStreamer() == 0) {
-      cl->AdoptStreamer(new TestProdStreamer<A, B>);
-    }
-  }
+  TestProd() : data() {}
   std::vector<size_t> data;
 };
 
 template <typename A, typename B>
 class TestProdStreamer : public TClassStreamer {
 public:
+  TestProdStreamer(std::ostream & os)
+    :
+    os_(os) {
+  }
+
   void operator()(TBuffer & R_b, void * objp) {
     static TClassRef cl(TClass::GetClass(typeid(TestProd<A, B>)));
     TestProd<A, B> *obj = reinterpret_cast<TestProd<A, B>*>(objp);
     if (R_b.IsReading()) {
-      std::cerr
-        << "Attempting to read stored object as a "
-        << cl->GetName()
-        << ".\n";
+      os_
+          << "Attempting to read stored object as a "
+          << cl->GetName()
+          << ".\n";
       cl->ReadBuffer(R_b, obj);
     }
     else {
-      std::cerr
-        << "Attempting to write a "
-        << cl->GetName()
-        << ".\n";
+      os_
+          << "Attempting to write a "
+          << cl->GetName()
+          << ".\n";
       cl->WriteBuffer(R_b, obj);
     }
   }
+private:
+  std::ostream & os_;
 };
 
 #endif /* test_Persistency_Provenance_RootClassMapping_t_h */
