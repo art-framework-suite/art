bea18f25e34c75adff078be1951c44f940ec3ba7
Chris Green <greenc@fnal.gov>
2011-09-21 17:55:19 -0500

    New function template metaDataTreeHasBranchFor.

--- art/Framework/IO/Root/setMetaDataBranchAddress.h
+++ art/Framework/IO/Root/setMetaDataBranchAddress.h
@@ -9,6 +9,9 @@ namespace art {
   template <typename T>
   void setMetaDataBranchAddress(TTree *tree,
                                 T *&t);
+
+  template <typename T>
+  bool metaDataTreeHasBranchFor(TTree *tree);
 }
 
 template <typename T>
@@ -16,6 +19,12 @@ inline
 void art::setMetaDataBranchAddress(TTree *tree,
                                    T *&t) {
   tree->SetBranchAddress(rootNames::metaBranchRootName<T>(), &t);
+}
+
+template <typename T>
+inline
+bool art::metaDataTreeHasBranchFor(TTree *tree) {
+  return tree->GetBranch(rootNames::metaBranchRootName<T>());
 }
 
 #endif /* art_Framework_IO_Root_setMetaDataBranchAddress_h */
