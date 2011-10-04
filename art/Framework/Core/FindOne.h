f60f4259ccd9107ba36df514190cb8c321c0316f
Chris Green <artists@fnal.gov>
2011-10-04 11:51:29 -0500

    Preparations for implementation of FindMany.
    Original commit messages:
    
    Pull out the main functionality and generalize for use by FindMany.
    
    Generalize ancillary data object handling for use by FindMany.
    
    Consistency.

--- art/Framework/Core/FindOne.h
+++ art/Framework/Core/FindOne.h
@@ -1,15 +1,12 @@
 #ifndef art_Framework_Core_FindOne_h
 #define art_Framework_Core_FindOne_h
 
-#include "art/Framework/Core/detail/getAssnsHandle.h"
+#include "art/Framework/Core/detail/IPRHelper.h"
+#include "art/Framework/Core/detail/ProductIDProvider.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art/Persistency/Common/Assns.h"
 #include "art/Persistency/Common/Ptr.h"
 #include "art/Utilities/InputTag.h"
-#include "art/Utilities/detail/metaprogramming.h"
-#include "art/Utilities/ensurePointer.h"
-#include "art/Utilities/pointersEqual.h"
-#include "cpp0x/type_traits"
 
 #include <vector>
 
@@ -21,183 +18,6 @@ namespace art {
   // Specialization.
   template <typename ProdB>
   class FindOne<ProdB, void>;
-
-  namespace detail {
-
-    struct ProductIDProvider;
-
-    struct ConstantProductIDProvider;
-
-    template <typename ProdA, typename ProdB, typename Data>
-    class OneFinder;
-
-    template <typename DATA, typename ASSNS, typename DATACOLL>
-    typename std::enable_if < !std::is_void<DATA>::value >::type
-    maybe_fill_data(ptrdiff_t assns_index,
-                    ASSNS const & assns,
-                    size_t data_index,
-                    DATACOLL & data)
-    {
-      data[data_index] = &assns.data(assns_index);
-    }
-
-    template <typename DATA, typename ASSNS, typename DATACOLL>
-    typename std::enable_if < std::is_void<DATA>::value >::type
-    maybe_fill_data(ptrdiff_t,
-                    ASSNS const &,
-                    size_t,
-                    DATACOLL &) { }
-  }
-
-  template <typename DATA, typename DATACOLL>
-  typename std::enable_if < !std::is_void<DATA>::value >::type
-  maybe_init_data(size_t size,
-                  DATACOLL & data)
-  {
-    data.assign(size, 0);
-  }
-
-  template <typename DATA, typename DATACOLL>
-  typename std::enable_if < std::is_void<DATA>::value >::type
-  maybe_init_data(size_t,
-                  DATACOLL &) { }
-}
-
-struct art::detail::ProductIDProvider {
-  template <typename T>
-  ProductID
-  operator()(T const & t) const { return t.id(); }
-};
-
-class art::detail::ConstantProductIDProvider {
-public:
-  ConstantProductIDProvider(ProductID const & pid) : pid_(pid) { }
-  template <typename T>
-  ProductID const &
-  operator()(T const &) const { return pid_; }
-  ProductID const &
-  operator()() const { return pid_; }
-private:
-  ProductID const & pid_;
-};
-
-template <typename ProdA, typename ProdB, typename Data>
-class art::detail::OneFinder {
-private:
-  class Def { };
-  //  typedef typename std::conditional<std::is_void<Data>::value, Def, typename FindOne<ProdB, Data>::dataColl_t>::type dataColl_t;
-  typedef typename std::conditional<std::is_void<Data>::value, Def, std::vector<Data const *> >::type dataColl_t;
-
-public:
-  OneFinder(Event const & e, InputTag const & tag) : event_(e), assnsTag_(tag) { }
-
-  // 1. Dispatches to one of the other two: use when dColl not wanted.
-  template <typename PidProvider, typename Acoll, typename Bcoll>
-  void
-  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl) const;
-
-  // 2. Algorithm useful when index of Acoll == index of desired Ptr in Assns.
-  template <typename PidProvider, typename Acoll, typename Bcoll>
-  typename std::enable_if <std::is_same<typename std::remove_cv<typename std::remove_pointer<typename Acoll::value_type>::type>::type, ProdA>::value >::type
-  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const;
-
-  // 3. Algorithm useful when dealing with collections of Ptrs.
-  template <typename PidProvider, typename Acoll, typename Bcoll>
-  typename std::enable_if<std::is_same<typename Acoll::value_type, Ptr<ProdA> >::value>::type
-  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const;
-
-private:
-  Event const & event_;
-  InputTag const & assnsTag_;
-};
-
-// 1.
-template <typename ProdA, typename ProdB, typename Data>
-template <typename PidProvider, typename Acoll, typename Bcoll>
-void
-art::detail::OneFinder<ProdA, ProdB, Data>::
-operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl) const
-{
-  Def dummy;
-  (*this)(aColl, pp, bColl, dummy);
-}
-
-// 2.
-template <typename ProdA, typename ProdB, typename Data>
-template <typename PidProvider, typename Acoll, typename Bcoll>
-typename std::enable_if <std::is_same<typename std::remove_cv<typename std::remove_pointer<typename Acoll::value_type>::type>::type, ProdA>::value >::type
-art::detail::OneFinder<ProdA, ProdB, Data>::
-operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const
-{
-  Handle<Assns<ProdA, ProdB, Data> > assnsHandle(detail::getAssnsHandle<ProdA, ProdB, Data>(event_, assnsTag_));
-  bColl.assign(aColl.size(), 0);
-  maybe_init_data<Data>(aColl.size(), dColl);
-  for (typename Assns<ProdA, ProdB, Data>::assn_iterator
-       beginAssns = assnsHandle->begin(),
-       itAssns = beginAssns,
-       endAssns = assnsHandle->end();
-       itAssns != endAssns;
-       ++itAssns) {
-    size_t aIndex(itAssns->first.key());
-    typename Acoll::const_iterator itA = aColl.begin();
-    std::advance(itA, aIndex);
-    if (itAssns->first.id() == pp() &&
-        pointersEqual(itAssns->first.get(), ensurePointer<typename Assns<ProdA, ProdB, Data>::assn_iterator::value_type::first_type::const_pointer>(itA))) {
-      if (bColl[aIndex]) {
-        throw Exception(errors::LogicError)
-            << "Attempted to create a FindOne object for a one-many or many-many"
-            << " association specified in collection "
-            << assnsTag_
-            << ".\n";
-      }
-      else {
-        bColl[aIndex] = itAssns->second.get();
-        maybe_fill_data<Data>(itAssns - beginAssns, *assnsHandle, aIndex, dColl);
-      }
-    }
-  }
-}
-
-// 3.
-template <typename ProdA, typename ProdB, typename Data>
-template <typename PidProvider, typename Acoll, typename Bcoll>
-typename std::enable_if<std::is_same<typename Acoll::value_type, art::Ptr<ProdA> >::value>::type
-art::detail::OneFinder<ProdA, ProdB, Data>::
-operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const
-{
-  Handle<Assns<ProdA, ProdB, Data> > assnsHandle(detail::getAssnsHandle<ProdA, ProdB, Data>(event_, assnsTag_));
-  bColl.assign(aColl.size(), 0);
-  maybe_init_data<Data>(aColl.size(), dColl);
-  for (typename Assns<ProdA, ProdB, Data>::assn_iterator
-       beginAssns = assnsHandle->begin(),
-       itAssns = beginAssns,
-       endAssns = assnsHandle->end();
-       itAssns != endAssns;
-       ++itAssns) {
-    size_t bIndex(0);
-    typename Acoll::const_iterator
-    i = aColl.begin(),
-    e = aColl.end();
-    // FIXME: Linear search: maybe we could do something more clever here?
-    for (; i != e; ++i, ++bIndex) {
-      if (itAssns->first.id() == pp(*i) &&
-          pointersEqual(itAssns->first.get(),
-                        ensurePointer<typename Acoll::value_type::const_pointer>(i))) {
-        if (bColl[bIndex]) {
-          throw Exception(errors::LogicError)
-              << "Attempted to create a FindOne object for a one-many or many-many"
-              << " association specified in collection "
-              << assnsTag_
-              << ".\n";
-        }
-        else {
-          bColl[bIndex] = itAssns->second.get();
-          maybe_fill_data<Data>(itAssns - beginAssns, *assnsHandle, bIndex, dColl);
-          break;
-        }
-      }
-    }
-  }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -240,9 +60,11 @@ public:
 
   bool operator == (FindOne<ProdB, void> const & other) const;
 
-private:
-protected: // Hopefully temporary.
+protected: 
   FindOne() : bCollection_() { }
+  bColl_t & bCollection() { return bCollection_; }
+
+private:
   bColl_t bCollection_;
 };
 
@@ -308,7 +130,7 @@ FindOne(Handle<ProdAColl> const & aCollection,
   bCollection_()
 {
   typedef typename ProdAColl::value_type ProdA;
-  detail::OneFinder<ProdA, ProdB, void> finder(e, tag);
+  detail::IPRHelper<ProdA, ProdB, void> finder(e, tag);
   finder(*aCollection, detail::ConstantProductIDProvider(aCollection.id()), bCollection_);
 }
 
@@ -322,7 +144,7 @@ FindOne(View<ProdA> const & view,
   bCollection_()
 {
   typename View<ProdA>::collection_type const & aCollection(view.vals());
-  detail::OneFinder<ProdA, ProdB, void> finder(e, tag);
+  detail::IPRHelper<ProdA, ProdB, void> finder(e, tag);
   finder(aCollection, detail::ConstantProductIDProvider(view.id()), bCollection_);
 }
 
@@ -336,7 +158,7 @@ FindOne(PtrProdAColl const & aPtrColl,
   bCollection_()
 {
   typedef typename PtrProdAColl::value_type::value_type ProdA;
-  detail::OneFinder<ProdA, ProdB, void> finder(e, tag);
+  detail::IPRHelper<ProdA, ProdB, void> finder(e, tag);
   finder(aPtrColl, detail::ProductIDProvider(), bCollection_);
 }
 
@@ -384,8 +206,8 @@ art::FindOne<ProdB, Data>::FindOne(Handle<ProdAColl> const & aCollection,
   dataCollection_()
 {
   typedef typename ProdAColl::value_type ProdA;
-  detail::OneFinder<ProdA, ProdB, Data> finder(e, tag);
-  finder(*aCollection, detail::ConstantProductIDProvider(aCollection.id()), base::bCollection_, dataCollection_);
+  detail::IPRHelper<ProdA, ProdB, Data> finder(e, tag);
+  finder(*aCollection, detail::ConstantProductIDProvider(aCollection.id()), base::bCollection(), dataCollection_);
 }
 
 template <typename ProdB, typename Data>
@@ -398,8 +220,8 @@ art::FindOne<ProdB, Data>::FindOne(View<ProdA> const & view,
   dataCollection_()
 {
   typename View<ProdA>::collection_type const & aCollection(view.vals());
-  detail::OneFinder<ProdA, ProdB, Data> finder(e, tag);
-  finder(aCollection, detail::ConstantProductIDProvider(view.id()), base::bCollection_, dataCollection_);
+  detail::IPRHelper<ProdA, ProdB, Data> finder(e, tag);
+  finder(aCollection, detail::ConstantProductIDProvider(view.id()), base::bCollection(), dataCollection_);
 }
 
 template <typename ProdB, typename Data>
@@ -412,8 +234,8 @@ art::FindOne<ProdB, Data>::FindOne(PtrProdAColl const & aPtrColl,
   dataCollection_()
 {
   typedef typename PtrProdAColl::value_type::value_type ProdA;
-  detail::OneFinder<ProdA, ProdB, Data> finder(e, tag);
-  finder(aPtrColl, detail::ProductIDProvider(), base::bCollection_, dataCollection_);
+  detail::IPRHelper<ProdA, ProdB, Data> finder(e, tag);
+  finder(aPtrColl, detail::ProductIDProvider(), base::bCollection(), dataCollection_);
 }
 
 template <typename ProdB, typename Data>
@@ -429,7 +251,7 @@ inline
 void
 art::FindOne<ProdB, Data>::get(size_type i, reference item, data_reference data) const
 {
-  item = base::bCollection_.at(i);
+  base::get(i, item);
   data = dataCollection_.at(i);
 }
 
---
+++ art/Framework/Core/detail/IPRHelper.h
@@ -0,0 +1,315 @@
+#ifndef art_Framework_Core_detail_IPRHelper_h
+#define art_Framework_Core_detail_IPRHelper_h
+
+// Helper class and associated gubbins for populating the FindOne and
+// FindMany query objects for inter-product references.
+
+#include "art/Framework/Core/detail/ProductIDProvider.h"
+#include "art/Framework/Core/detail/getAssnsHandle.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Persistency/Common/Assns.h"
+#include "art/Persistency/Common/Ptr.h"
+#include "art/Utilities/Exception.h"
+#include "art/Utilities/InputTag.h"
+#include "art/Utilities/ensurePointer.h"
+#include "art/Utilities/pointersEqual.h"
+#include "cpp0x/type_traits"
+
+namespace art {
+  namespace detail {
+
+    class IPRHelperDef { };
+
+    template <typename ProdA, typename ProdB, typename Data>
+    class IPRHelper;
+
+    template <typename DATA>
+    class DataCollHelper {
+    public:
+      DataCollHelper(InputTag const & assnsTag);
+
+      void init(size_t size,
+                std::vector<DATA const *> & data) const;
+      template <typename ASSNS>
+      void fill(ptrdiff_t assns_index,
+                ASSNS const & assns,
+                size_t data_index,
+                std::vector<DATA const *> & data) const;
+
+      void init(size_t size,
+                std::vector<std::vector<DATA const *> > & data) const;
+      template <typename ASSNS>
+      void fill(ptrdiff_t assns_index,
+                ASSNS const & assns,
+                size_t data_index,
+                std::vector<std::vector<DATA const *> > & data) const;
+
+      void init(size_t, IPRHelperDef &) const;
+      template <typename ASSNS>
+      void fill(ptrdiff_t,
+                ASSNS const &,
+                size_t,
+                IPRHelperDef &) const;
+
+    private:
+      InputTag const & assnsTag_;
+    };
+
+    template <typename ProdB>
+    class BcollHelper {
+    public:
+      BcollHelper(InputTag const & assnsTag);
+
+      void init(size_t size, std::vector<ProdB const *> & bColl) const;
+      void fill(size_t index, ProdB const * item, std::vector<ProdB const *> & bColl) const;
+
+      void init(size_t size, std::vector<std::vector<ProdB const *> >  & bColl) const;
+      void fill(size_t index, ProdB const * item, std::vector<std::vector<ProdB const *> >  & bColl) const;
+
+    private:
+      InputTag const & assnsTag_;
+    };
+  }
+}
+
+template <typename ProdA, typename ProdB, typename Data>
+class art::detail::IPRHelper {
+private:
+  //  typedef typename std::conditional<std::is_void<Data>::value, IPRHelperDef, typename FindOne<ProdB, Data>::dataColl_t>::type dataColl_t;
+  typedef typename std::conditional<std::is_void<Data>::value, IPRHelperDef, std::vector<Data const *> >::type dataColl_t;
+
+public:
+  IPRHelper(Event const & e, InputTag const & tag) : event_(e), assnsTag_(tag) { }
+
+  // 1. Dispatches to one of the other two: use when dColl not wanted.
+  template <typename PidProvider, typename Acoll, typename Bcoll>
+  void
+  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl) const;
+
+  // 2. Algorithm useful when index of Acoll == index of desired Ptr in Assns.
+  template <typename PidProvider, typename Acoll, typename Bcoll>
+  typename std::enable_if <std::is_same<typename std::remove_cv<typename std::remove_pointer<typename Acoll::value_type>::type>::type, ProdA>::value >::type
+  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const;
+
+  // 3. Algorithm useful when dealing with collections of Ptrs.
+  template <typename PidProvider, typename Acoll, typename Bcoll>
+  typename std::enable_if<std::is_same<typename Acoll::value_type, Ptr<ProdA> >::value>::type
+  operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const;
+
+private:
+  Event const & event_;
+  InputTag const & assnsTag_;
+};
+
+// 1.
+template <typename ProdA, typename ProdB, typename Data>
+template <typename PidProvider, typename Acoll, typename Bcoll>
+void
+art::detail::IPRHelper<ProdA, ProdB, Data>::
+operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl) const
+{
+  IPRHelperDef dummy;
+  (*this)(aColl, pp, bColl, dummy);
+}
+
+// 2.
+template <typename ProdA, typename ProdB, typename Data>
+template <typename PidProvider, typename Acoll, typename Bcoll>
+typename std::enable_if <std::is_same<typename std::remove_cv<typename std::remove_pointer<typename Acoll::value_type>::type>::type, ProdA>::value >::type
+art::detail::IPRHelper<ProdA, ProdB, Data>::
+operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const
+{
+  detail::BcollHelper<ProdB> bh(assnsTag_);
+  detail::DataCollHelper<Data> dh(assnsTag_);
+  Handle<Assns<ProdA, ProdB, Data> > assnsHandle(detail::getAssnsHandle<ProdA, ProdB, Data>(event_, assnsTag_));
+  bh.init(aColl.size(), bColl);
+  dh.init(aColl.size(), dColl);
+  for (typename Assns<ProdA, ProdB, Data>::assn_iterator
+       beginAssns = assnsHandle->begin(),
+       itAssns = beginAssns,
+       endAssns = assnsHandle->end();
+       itAssns != endAssns;
+       ++itAssns) {
+    size_t aIndex(itAssns->first.key());
+    typename Acoll::const_iterator itA = aColl.begin();
+    std::advance(itA, aIndex);
+    if (itAssns->first.id() == pp() &&
+        pointersEqual(itAssns->first.get(), ensurePointer<typename Assns<ProdA, ProdB, Data>::assn_iterator::value_type::first_type::const_pointer>(itA))) {
+      bh.fill(aIndex, itAssns->second.get(), bColl);
+      dh.fill(itAssns - beginAssns, *assnsHandle, aIndex, dColl);
+    }
+  }
+}
+
+// 3.
+template <typename ProdA, typename ProdB, typename Data>
+template <typename PidProvider, typename Acoll, typename Bcoll>
+typename std::enable_if<std::is_same<typename Acoll::value_type, art::Ptr<ProdA> >::value>::type
+art::detail::IPRHelper<ProdA, ProdB, Data>::
+operator()(Acoll const & aColl, PidProvider const & pp, Bcoll & bColl, dataColl_t & dColl) const
+{
+  detail::BcollHelper<ProdB> bh(assnsTag_);
+  detail::DataCollHelper<Data> dh(assnsTag_);
+  Handle<Assns<ProdA, ProdB, Data> > assnsHandle(detail::getAssnsHandle<ProdA, ProdB, Data>(event_, assnsTag_));
+  bh.init(aColl.size(), bColl);
+  dh.init(aColl.size(), dColl);
+  for (typename Assns<ProdA, ProdB, Data>::assn_iterator
+       beginAssns = assnsHandle->begin(),
+       itAssns = beginAssns,
+       endAssns = assnsHandle->end();
+       itAssns != endAssns;
+       ++itAssns) {
+    size_t bIndex(0);
+    // FIXME: Linear search: maybe we could do something more clever here?
+    for (typename Acoll::const_iterator
+         i = aColl.begin(),
+         e = aColl.end();
+         i != e;
+         ++i, ++bIndex) {
+      if (itAssns->first.id() == pp(*i) &&
+          pointersEqual(itAssns->first.get(),
+                        ensurePointer<typename Acoll::value_type::const_pointer>(i))) {
+        bh.fill(bIndex, itAssns->second.get(), bColl);
+        dh.fill(itAssns - beginAssns, *assnsHandle, bIndex, dColl);
+        break;
+      }
+    }
+  }
+}
+
+template <typename DATA>
+inline
+art::detail::DataCollHelper<DATA>::
+DataCollHelper(InputTag const & assnsTag)
+  :
+  assnsTag_(assnsTag)
+{
+}
+
+template <typename DATA>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+init(size_t size,
+     std::vector<DATA const *> & data) const
+{
+  data.assign(size, 0);
+}
+
+template <typename DATA>
+template <typename ASSNS>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+fill(ptrdiff_t assns_index,
+     ASSNS const & assns,
+     size_t data_index,
+     std::vector<DATA const *> & data) const
+{
+  // Check not necessary as this always occurs after bColl filling, which is checked.
+  data[data_index] = &assns.data(assns_index);
+}
+
+template <typename DATA>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+init(size_t size,
+     std::vector<std::vector<DATA const *> > & data) const
+{
+  data.resize(size);
+}
+
+template <typename DATA>
+template <typename ASSNS>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+fill(ptrdiff_t assns_index,
+     ASSNS const & assns,
+     size_t data_index,
+     std::vector<std::vector<DATA const *> > & data) const
+{
+  data[data_index].push_back(assns.data(assns_index));
+}
+
+template <typename DATA>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+init(size_t, IPRHelperDef &) const
+{
+}
+
+template <typename DATA>
+template <typename ASSNS>
+inline
+void
+art::detail::DataCollHelper<DATA>::
+fill(ptrdiff_t,
+     ASSNS const &,
+     size_t,
+     IPRHelperDef &) const
+{
+}
+
+template <typename ProdB>
+inline
+art::detail::BcollHelper<ProdB>::
+BcollHelper(InputTag const & assnsTag)
+  :
+  assnsTag_(assnsTag)
+{
+}
+
+template <typename ProdB>
+inline
+void
+art::detail::BcollHelper<ProdB>::
+init(size_t size, std::vector<ProdB const *> & bColl) const
+{
+  bColl.assign(size, 0);
+}
+
+template <typename ProdB>
+void
+art::detail::BcollHelper<ProdB>::
+fill(size_t index, ProdB const * item, std::vector<ProdB const *> & bColl) const
+{
+  if (bColl[index]) {
+    throw Exception(errors::LogicError)
+        << "Attempted to create a FindOne object for a one-many or many-many"
+        << " association specified in collection "
+        << assnsTag_
+        << ".\n";
+  }
+  else {
+    bColl[index] = item;
+  }
+}
+
+template <typename ProdB>
+inline
+void
+art::detail::BcollHelper<ProdB>::
+init(size_t size, std::vector<std::vector<ProdB const *> >  & bColl) const
+{
+  bColl.resize(size);
+}
+
+template <typename ProdB>
+inline
+void
+art::detail::BcollHelper<ProdB>::
+fill(size_t index,
+     ProdB const * item,
+     std::vector<std::vector<ProdB const *> >  & bColl) const
+{
+  bColl[index].push_back(item);
+}
+
+#endif /* art_Framework_Core_detail_IPRHelper_h */
+
+// Local Variables:
+// mode: c++
+// End:
