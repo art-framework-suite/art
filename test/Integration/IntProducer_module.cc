c4f27508b20016cacbf543d126555bd4c50867f2
Chris Green <artists@fnal.gov>
2011-10-17 11:38:03 -0500

    IntProducer can now place products in Run or SubRun instead.

--- test/Integration/IntProducer_module.cc
+++ test/Integration/IntProducer_module.cc
@@ -5,12 +5,17 @@
 //--------------------------------------------------------------------
 
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Principal/Event.h"
 #include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
 #include "art/Framework/Principal/Handle.h"
+#include "art/Persistency/Provenance/BranchType.h"
 #include "cpp0x/memory"
 #include "fhiclcpp/ParameterSet.h"
+
 #include "test/TestObjects/ToyProducts.h"
+
 #include <iostream>
 
 namespace arttest {
@@ -24,9 +29,26 @@ class arttest::IntProducer
 {
 public:
   explicit IntProducer( fhicl::ParameterSet const& p )
-  : value_( p.get<int>("ivalue") )
+    : value_( p.get<int>("ivalue") ),
+      // enums don't usually have a conversion from string
+      branchType_( art::BranchType(p.get<unsigned long>("branchType", art::InEvent)) )
   {
-    produces<IntProduct>();
+    switch (branchType_) {
+    case art::InEvent:
+      produces<IntProduct>();
+      break;
+    case art::InSubRun:
+      produces<IntProduct, art::InSubRun>();
+      break;
+    case art::InRun:
+      produces<IntProduct, art::InRun>();
+      break;
+    default:
+      throw art::Exception(art::errors::LogicError)
+        << "Unknown branch type "
+        << branchType_
+        << ".\n";
+    }
   }
 
   explicit IntProducer( int i )
@@ -38,17 +60,45 @@ public:
   virtual ~IntProducer() { }
 
   virtual void produce( art::Event& e );
+  virtual void endSubRun( art::SubRun & sr );
+  virtual void endRun( art::Run& r );
 
 private:
+
+  template <typename PUTTER>
+  void put(PUTTER & p);
+
   int value_;
+  art::BranchType branchType_;
+  
 };  // IntProducer
 
 void
 IntProducer::produce( art::Event& e )
 {
   std::cerr << "Holy cow, IntProducer::produce is running!\n";
-  std::auto_ptr<IntProduct> p(new IntProduct(value_));
-  e.put(p);
+  if (branchType_ == art::InEvent) put(e);
+}
+
+void
+IntProducer::endSubRun( art::SubRun& sr )
+{
+  std::cerr << "Holy cow, IntProducer::endSubRun is running!\n";
+  if (branchType_ == art::InSubRun) put(sr);
+}
+
+void
+IntProducer::endRun( art::Run& r )
+{
+  std::cerr << "Holy cow, IntProducer::endRun is running!\n";
+  if (branchType_ == art::InRun) put(r);
+}
+
+template <typename PUTTER>
+void
+IntProducer::put(PUTTER & p) {
+  std::auto_ptr<IntProduct> prod(new IntProduct(value_));
+  p.put(prod);
 }
 
 DEFINE_ART_MODULE(IntProducer);
