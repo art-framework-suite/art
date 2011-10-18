6ef8a485973f9114b4b3c23d98b7e52b24114602
Chris Green <artists@fnal.gov>
2011-10-18 07:33:27 -0500

    Ignore more files.

--- tools/migration/update_sources.sh
+++ tools/migration/update_sources.sh
@@ -134,7 +134,7 @@ if [[ -n "${file}" ]]; then
   fi
 else
   for F in `find $TOP \( -type d \( -name .git -o -name .svn -o -name CVS -o -name ups -o -path '*/tools/migration' -o -path '*/art/ParameterSet' \) \
-                       -prune \) -o -type f \! \( -name '*~' -o -name '*.bak' -o -name '*.new' -o -name '*.gdml' -o -name '*.py' -o -name '*.sh' \) -print`; do
+                       -prune \) -o -type f \! \( -name '*~' -o -name '*.bak' -o -name '*.orig' -o -name '*.new' -o -name '*.gdml' -o -name '*.py' -o -name '*.sh' -o -name '*.o' -o -name '*.so' -o -name '*.os' -o -name '*.root' -o -name 'SConscript'\) -print`; do
     one_file "$F"
   done
 fi
