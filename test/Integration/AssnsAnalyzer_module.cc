0c051cc323582fb30762049025ec21412abb6606
Chris Green <artists@fnal.gov>
2012-01-11 11:18:05 -0600

    Make versions of FindOne and FindMany that return Ptr, maximizing code reuse.

--- test/Integration/AssnsAnalyzer_module.cc
+++ test/Integration/AssnsAnalyzer_module.cc
@@ -7,22 +7,29 @@
 // from art v0_07_12.
 ////////////////////////////////////////////////////////////////////////
 
-#include <boost/test/included/unit_test.hpp>
-
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/FindMany.h"
+#include "art/Framework/Core/FindManyP.h"
 #include "art/Framework/Core/FindOne.h"
+#include "art/Framework/Core/FindOneP.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/View.h"
 #include "art/Persistency/Common/Assns.h"
 #include "art/Persistency/Common/Ptr.h"
 #include "art/Persistency/Common/PtrVector.h"
+#include "cetlib/maybe_ref.h"
+#include "cpp0x/type_traits"
 #include "test/TestObjects/AssnTestData.h"
 
-class AssnsAnalyzer;
+#include <boost/test/included/unit_test.hpp>
+#include <boost/type_traits.hpp>
 
-class AssnsAnalyzer : public art::EDAnalyzer {
+namespace arttest {
+   class AssnsAnalyzer;
+}
+
+class arttest::AssnsAnalyzer : public art::EDAnalyzer {
 public:
   explicit AssnsAnalyzer(fhicl::ParameterSet const & p);
   virtual ~AssnsAnalyzer();
@@ -30,6 +37,12 @@ public:
   virtual void analyze(art::Event const & e);
 
 private:
+  template <template <typename, typename> class FO>
+  void testOne(art::Event const & e) const;
+
+  template <template <typename, typename> class FM>
+  void testMany(art::Event const & e) const;
+
   std::string inputLabel_;
   bool testAB_;
   bool testBA_;
@@ -42,9 +55,24 @@ namespace {
   typedef art::Assns<std::string, size_t, arttest::AssnTestData> AssnsBA_t;
   typedef art::Assns<size_t, std::string> AssnsABV_t;
   typedef art::Assns<std::string, size_t> AssnsBAV_t;
+
+  // Common case, can dereference (eg Ptr<T>).
+  template <typename T, template <typename> class WRAP>
+  typename std::enable_if<boost::has_dereference<WRAP<T> >::value, T const &>::type
+  dereference(WRAP<T> const & wrapper) {
+     return *wrapper;
+  }
+
+  // maybe_ref<T>.
+  template <typename T, template <typename> class WRAP>
+  typename std::enable_if<std::is_same<WRAP<T>, cet::maybe_ref<T> >::value, T const &>::type
+  dereference(WRAP<T> const & wrapper) {
+     return wrapper.ref();
+  }
 }
 
-AssnsAnalyzer::AssnsAnalyzer(fhicl::ParameterSet const & p)
+arttest::AssnsAnalyzer::
+AssnsAnalyzer(fhicl::ParameterSet const & p)
   :
   inputLabel_(p.get<std::string>("input_label")),
   testAB_(p.get<bool>("test_AB", true)),
@@ -52,11 +80,24 @@ AssnsAnalyzer::AssnsAnalyzer(fhicl::ParameterSet const & p)
 {
 }
 
-AssnsAnalyzer::~AssnsAnalyzer()
+arttest::AssnsAnalyzer::
+~AssnsAnalyzer()
 {
 }
 
-void AssnsAnalyzer::analyze(art::Event const & e)
+void
+arttest::AssnsAnalyzer::analyze(art::Event const & e)
+{
+  testOne<art::FindOne>(e);
+  testOne<art::FindOneP>(e);
+  testMany<art::FindMany>(e);
+  testMany<art::FindManyP>(e);
+}
+
+template <template <typename, typename> class FO>
+void
+arttest::AssnsAnalyzer::
+testOne(art::Event const & e) const
 {
   static char const * const x[] = { "zero", "one", "two" };
   static char const * const a[] = { "A", "B", "C" };
@@ -78,15 +119,15 @@ void AssnsAnalyzer::analyze(art::Event const & e)
     BOOST_REQUIRE(e.getByLabel(inputLabel_, hBAV));
     BOOST_REQUIRE_EQUAL(hBAV->size(), 3u);
   }
-  // Construct a FindOne using a handle to a collection.
+  // Construct a FO using a handle to a collection.
   art::Handle<std::vector<size_t> > hAcoll;
   BOOST_REQUIRE(e.getByLabel(inputLabel_, hAcoll));
-  art::FindOne<std::string, arttest::AssnTestData> foB(hAcoll, e, inputLabel_);
-  art::FindOne<std::string> foBV(hAcoll, e, inputLabel_);
+  FO<std::string, arttest::AssnTestData> foB(hAcoll, e, inputLabel_);
+  FO<std::string, void> foBV(hAcoll, e, inputLabel_);
   art::Handle<std::vector<std::string> > hBcoll;
   BOOST_REQUIRE(e.getByLabel(inputLabel_, hBcoll));
-  art::FindOne<size_t, arttest::AssnTestData> foA(hBcoll, e, inputLabel_);
-  art::FindOne<size_t> foAV(hBcoll, e, inputLabel_);
+  FO<size_t, arttest::AssnTestData> foA(hBcoll, e, inputLabel_);
+  FO<size_t, void> foAV(hBcoll, e, inputLabel_);
   for (size_t i = 0; i < 3; ++i) {
     if (testAB_) {
       BOOST_CHECK_EQUAL(*(*hAB)[i].first, i);
@@ -107,14 +148,14 @@ void AssnsAnalyzer::analyze(art::Event const & e)
       BOOST_CHECK_EQUAL(*(*hBAV)[i].second, i);
     }
     // Check FindOne.
-    BOOST_CHECK_EQUAL(foB.at(i).ref(), std::string(x[ai[i]]));
-    BOOST_CHECK_EQUAL(foB.data(i).ref().d1, i);
-    BOOST_CHECK_EQUAL(foB.data(i).ref().d2, bi[i]);
-    BOOST_CHECK_EQUAL(foBV.at(i).ref(), std::string(x[ai[i]]));
-    BOOST_CHECK_EQUAL(foA.at(i).ref(), bi[i]);
-    BOOST_CHECK_EQUAL(foA.data(i).ref().d1, ai[i]);
-    BOOST_CHECK_EQUAL(foA.data(i).ref().d2, i);
-    BOOST_CHECK_EQUAL(foAV.at(i).ref(), bi[i]);
+    BOOST_CHECK_EQUAL(dereference(foB.at(i)), std::string(x[ai[i]]));
+    BOOST_CHECK_EQUAL(dereference(foB.data(i)).d1, i);
+    BOOST_CHECK_EQUAL(dereference(foB.data(i)).d2, bi[i]);
+    BOOST_CHECK_EQUAL(dereference(foBV.at(i)), std::string(x[ai[i]]));
+    BOOST_CHECK_EQUAL(dereference(foA.at(i)), bi[i]);
+    BOOST_CHECK_EQUAL(dereference(foA.data(i)).d1, ai[i]);
+    BOOST_CHECK_EQUAL(dereference(foA.data(i)).d2, i);
+    BOOST_CHECK_EQUAL(dereference(foAV.at(i)), bi[i]);
   }
   // Check alternative accessors and range checking for Assns.
   if (testAB_) {
@@ -130,17 +171,17 @@ void AssnsAnalyzer::analyze(art::Event const & e)
   // Check FindOne on View.
   art::View<A_t> va;
   BOOST_REQUIRE(e.getView(inputLabel_, va));
-  art::FindOne<B_t, arttest::AssnTestData> foBv(va, e, inputLabel_);
+  FO<B_t, arttest::AssnTestData> foBv(va, e, inputLabel_);
   BOOST_REQUIRE(foB == foBv);
   art::View<B_t> vb;
   BOOST_REQUIRE(e.getView(inputLabel_, vb));
-  art::FindOne<A_t, arttest::AssnTestData> foAv(vb, e, inputLabel_);
+  FO<A_t, arttest::AssnTestData> foAv(vb, e, inputLabel_);
   BOOST_REQUIRE(foA == foAv);
   // Check FindOne on PtrVector.
   va.vals()[1] = 0;
   art::PtrVector<A_t> pva;
   va.fill(pva);
-  art::FindOne<B_t, arttest::AssnTestData> foBpv(pva, e, inputLabel_);
+  FO<B_t, arttest::AssnTestData> foBpv(pva, e, inputLabel_);
   BOOST_CHECK_EQUAL(foBpv.at(0), foB.at(0));
   BOOST_CHECK_EQUAL(foBpv.data(0), foB.data(0));
   BOOST_CHECK_EQUAL(foBpv.at(1), foB.at(2)); // Knocked out the middle.
@@ -148,7 +189,7 @@ void AssnsAnalyzer::analyze(art::Event const & e)
   vb.vals()[1] = 0;
   art::PtrVector<B_t> pvb;
   vb.fill(pvb);
-  art::FindOne<A_t, arttest::AssnTestData> foApv(pvb, e, inputLabel_);
+  FO<A_t, arttest::AssnTestData> foApv(pvb, e, inputLabel_);
   BOOST_CHECK_EQUAL(foApv.at(0), foA.at(0));
   BOOST_CHECK_EQUAL(foApv.data(0), foA.data(0));
   BOOST_CHECK_EQUAL(foApv.at(1), foA.at(2)); // Knocked out the middle.
@@ -156,19 +197,28 @@ void AssnsAnalyzer::analyze(art::Event const & e)
   // Check for range errors.
   BOOST_CHECK_THROW(foApv.at(3), std::out_of_range);
   BOOST_CHECK_THROW(foApv.data(3), std::out_of_range);
+  BOOST_CHECK_THROW((FO<B_t, arttest::AssnTestData> (hAcoll, e, art::InputTag(inputLabel_, "M"))), art::Exception);
+}
+
+template <template <typename, typename> class FM>
+void
+arttest::AssnsAnalyzer::
+testMany(art::Event const & e) const
+{
+  art::Handle<std::vector<size_t> > hAcoll;
+  BOOST_REQUIRE(e.getByLabel(inputLabel_, hAcoll));
   // Check FindMany.
-  BOOST_CHECK_THROW((art::FindOne<B_t, arttest::AssnTestData> (hAcoll, e, art::InputTag(inputLabel_, "M"))), art::Exception);
-  art::FindMany<B_t, arttest::AssnTestData> fmB(hAcoll, e, art::InputTag(inputLabel_, "M"));
+  FM<B_t, arttest::AssnTestData> fmB(hAcoll, e, art::InputTag(inputLabel_, "M"));
   BOOST_CHECK_EQUAL(fmB.at(0).size(), 1u);
   BOOST_CHECK_EQUAL(fmB.at(1).size(), 2u);
   BOOST_CHECK_EQUAL(fmB.at(2).size(), 1u);
   BOOST_CHECK_EQUAL(fmB.data(0).size(), 1u);
   BOOST_CHECK_EQUAL(fmB.data(1).size(), 2u);
   BOOST_CHECK_EQUAL(fmB.data(2).size(), 1u);
-  art::FindMany<B_t> fmBV(hAcoll, e, art::InputTag(inputLabel_, "M"));
+  FM<B_t, void> fmBV(hAcoll, e, art::InputTag(inputLabel_, "M"));
   BOOST_CHECK_EQUAL(fmBV.at(0).size(), 1u);
   BOOST_CHECK_EQUAL(fmBV.at(1).size(), 2u);
   BOOST_CHECK_EQUAL(fmBV.at(2).size(), 1u);
 }
 
-DEFINE_ART_MODULE(AssnsAnalyzer)
+DEFINE_ART_MODULE(arttest::AssnsAnalyzer)
