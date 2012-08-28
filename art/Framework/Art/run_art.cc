14b91c5217dbfdac6c480e429424956a1ddd923a
Paul Russo <russo@cluck.fnal.gov>
2012-08-28 13:49:06 -0500

    Abstract service implementation, part 1.
    Remove vestigial header.

--- art/Framework/Art/run_art.cc
+++ art/Framework/Art/run_art.cc
@@ -7,7 +7,6 @@
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Registry/ServiceRegistry.h"
 #include "art/Framework/Services/Registry/ServiceToken.h"
-#include "art/Framework/Services/Registry/ServiceWrapper.h"
 #include "art/Framework/Art/InitRootHandlers.h"
 #include "art/Utilities/ExceptionMessages.h"
 #include "art/Utilities/RootHandlers.h"
