fde2c39d88f640b89abe71c21bb4137758f8a393
Marc Paterno <paterno@fnal.gov>
2012-09-14 20:51:53 -0500

    Remove unused typedef

--- art/Framework/Core/ProcessingTask.h
+++ art/Framework/Core/ProcessingTask.h
@@ -1,15 +1,1 @@
-#ifndef art_Framework_Core_ProcessingTask_h
-#define art_Framework_Core_ProcessingTask_h
-
-#include <string>
-
-namespace art
-{
-  typedef std::string ProcessingTask;
-}
-
-#endif /* art_Framework_Core_ProcessingTask_h */
-
-// Local Variables:
-// mode: c++
-// End:
+#error This file is obsolete
