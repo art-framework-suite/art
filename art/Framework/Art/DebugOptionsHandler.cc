82c1c6f857d65beab117df715c4e593314d22835
Chris Green <greenc@fnal.gov>
2012-09-27 13:48:24 -0500

    Fix problems found by testing.

--- art/Framework/Art/DebugOptionsHandler.cc
+++ art/Framework/Art/DebugOptionsHandler.cc
@@ -39,6 +39,17 @@ art::DebugOptionsHandler::
 doProcessOptions(bpo::variables_map const & vm,
                  fhicl::intermediate_table & raw_config)
 {
+  if (vm.count("trace") == 1 &&
+      vm.count("notrace") == 1) {
+    throw Exception(errors::Configuration)
+        << "Options --trace and --notrace are incompatible.\n";
+  }
+  if (vm.count("memcheck") == 1 &&
+      vm.count("nomemcheck") == 1) {
+    throw Exception(errors::Configuration)
+        << "Options --memcheck and --nomemcheck are incompatible.\n";
+  }
+
   if (vm.count("trace")) {
     raw_config.put("services.scheduler.wantTracer", true);
   }
@@ -51,11 +62,11 @@ doProcessOptions(bpo::variables_map const & vm,
   else if (vm.count("nomemcheck")) {
     raw_config.erase("services.SimpleMemoryCheck");
   }
-  if (vm.count("rethrow-all") == 1 &&
+  if (vm.count("rethrow-all") == 1 ||
       vm.count("rethrow-default") == 1) {
     raw_config.put("services.scheduler.defaultExceptions", false);
     if (vm.count("rethrow-all") == 1) {
-      raw_config.putEmptyTable("services.scheduler.IgnoreCompletely");
+      raw_config.putEmptySequence("services.scheduler.IgnoreCompletely");
       raw_config.putEmptySequence("services.scheduler.SkipEvent");
       raw_config.putEmptySequence("services.scheduler.FaileModule");
       raw_config.putEmptySequence("services.scheduler.FailPath");
