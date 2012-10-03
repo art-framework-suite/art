83db1db0d65ebcb917771bd30891498f6c80c873
Chris Green <greenc@fnal.gov>
2012-10-02 22:48:40 -0500

    Replace missing header.

--- test/Integration/classes.h
+++ test/Integration/classes.h
@@ -1,9 +1,11 @@
 #include "art/Persistency/Common/Assns.h"
 #include "art/Persistency/Common/Ptr.h"
+#include "art/Persistency/Common/PtrVector.h"
 #include "art/Persistency/Common/Wrapper.h"
 #include "cetlib/map_vector.h"
 #include "cpp0x/utility"
 #include "test/TestObjects/AssnTestData.h"
+
 #include <string>
 #include <vector>
 
