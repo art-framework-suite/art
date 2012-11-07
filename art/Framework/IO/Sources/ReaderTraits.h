26cb524a8b4295c25d1645014b2d6a9dc9986c33
Chris Green <artists@fnal.gov>
2012-11-07 16:16:03 -0600

    Implement the "as-you-were" part of the solution.

--- art/Framework/IO/Sources/ReaderTraits.h
+++ art/Framework/IO/Sources/ReaderTraits.h
@@ -9,11 +9,24 @@
 // Specialize as required.
 //
 ////////////////////////////////////////////////////////////////////////
+
 namespace art {
+  // We are a generator, not a reader (or, we read our data from
+  // somewhere other than files specified by source.fileNames).
   template <typename DETAIL>
-    struct Reader_wantFileServices {
-      static constexpr bool value = true;
-    };
+  struct Reader_generator {
+    static constexpr bool value = false;
+  };
+
+  // Use the standard service interfaces (CatalogInterface and
+  // FileTransfer) to obtain files.
+  template <typename DETAIL>
+  struct Reader_wantFileServices {
+    // If you're a generator, you almost certainly don't want to use the
+    // standard file services.
+    static constexpr bool value = !Reader_generator<DETAIL>::value;
+  };
+
 }
 #endif /* art_Framework_IO_Sources_ReaderTraits_h */
 
