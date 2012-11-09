9926c960f7309d14864113c885eafec41ba95e02
Chris Green <artists@fnal.gov>
2012-11-08 20:50:50 -0600

    Rationalize enumerated types and provide translate functions.

--- art/Framework/Services/Interfaces/FileDisposition.h
+++ art/Framework/Services/Interfaces/FileDisposition.h
@@ -1,15 +1,23 @@
 #ifndef art_Framework_Services_Interfaces_FileDisposition_h
 #define art_Framework_Services_Interfaces_FileDisposition_h
 
+#include <string>
+
 namespace art {
   enum class FileDisposition;
+
+  // Translate enum to string;
+  std::string translateFileDisposition(FileDisposition fd);
+
 }
 
 enum class art::FileDisposition {
-  TRANSFERRED,
-  CONSUMED,
-  SKIPPED
-};
+  PENDING = -1,
+    TRANSFERRED,
+    CONSUMED,
+    SKIPPED,
+    INCOMPLETE
+            };
 
 #endif /* art_Framework_Services_Interfaces_FileDisposition_h */
 
--- art/Framework/Services/Interfaces/FileTransferStatus.h
+++ art/Framework/Services/Interfaces/FileTransferStatus.h
@@ -1,29 +1,34 @@
 #ifndef art_Framework_Services_Interfaces_FileTransferStatus_h
 #define art_Framework_Services_Interfaces_FileTransferStatus_h
 
+#include <string>
+
 namespace art {
 
   namespace detail {
     namespace FTS {
       enum FileTransferStatus {
-        CREATED         = 201,  // The only normal return from file transfer protocol
-        OK              = 200,
-        BADREQUEST      = 400,
-        UNAUTHORIZED    = 401,
-        PAYMENTREQUIRED = 402,
-        FORBIDDEN       = 403,
-        NOTFOUND        = 404,
-        GONE            = 410,
-        TOOLARGE        = 413,
-        URITOOLONG      = 404,
-        SERVERERROR     = 500,
-        UNAVAILABLE     = 503
+        PENDING = -1, // Not attempted yet.
+        SUCCESS = 0, // A normal return from delivery protocol
+        BAD_REQUEST = 400,
+        UNAUTHORIZED = 401,
+        PAYMENT_REQUIRED = 402,
+        FORBIDDEN = 403,
+        NOT_FOUND = 404,
+        GONE = 410,
+        TOO_LARGE = 413,
+        URI_TOO_LONG = 414,
+        SERVER_ERROR = 500,
+        UNAVAILABLE = 503
       };
     }
   }
 
   // Enum values must be scoped, eg FileDeliveryStatus::OK.
   using detail::FTS::FileTransferStatus;
+
+  // Translate enum to string;
+  std::string translateFileTransferStatus(FileTransferStatus stat);
 
 } // end of art namespace
 
