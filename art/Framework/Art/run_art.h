04045cc2bbf4c1573abf0d2c93fa3c3416e14502
Kurt Biery <biery@fnal.gov>
2013-01-02 08:36:08 -0600

    Created a new function to accept the configuration as an input string.

--- art/Framework/Art/run_art.h
+++ art/Framework/Art/run_art.h
@@ -10,6 +10,8 @@ namespace art {
               bpo::options_description & all_desc,
               cet::filepath_maker & lookupPolicy,
               art::OptionsHandlers && handlers);
+
+  int run_art_string_config(const std::string& config_string);
 }
 #endif
 #endif /* art_Framework_Art_run_art_h */
