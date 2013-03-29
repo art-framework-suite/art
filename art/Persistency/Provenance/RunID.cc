24bb06141a676fbd0116b11e35438c8d8831fd7e
Chris Green <artists@fnal.gov>
2013-03-29 10:32:53 -0500

    RunID now has the concept of a flushRun; modernize.

--- art/Persistency/Provenance/RunID.cc
+++
@@ -1,8 +0,0 @@
-#include "art/Persistency/Provenance/RunID.h"
-
-art::RunNumber_t const art::RunID::INVALID_RUN_NUMBER = -1;
-art::RunNumber_t const art::RunID::MAX_VALID_RUN_NUMBER = -2;
-art::RunNumber_t const art::RunID::FIRST_RUN_NUMBER = 1;
-// art::RunNumber_t const art::RunID::INVALID_RUN_NUMBER = 0;
-// art::RunNumber_t const art::RunID::MAX_VALID_RUN_NUMBER = -1;
-// art::RunNumber_t const art::RunID::FIRST_RUN_NUMBER = 1;
