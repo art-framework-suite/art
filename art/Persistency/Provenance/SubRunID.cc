de55945f09514e1496de4408c721128efa5ce895
Chris Green <artists@fnal.gov>
2013-03-29 12:03:28 -0500

    SubRunID now has the concept of a flushSubRun; modernize.

--- art/Persistency/Provenance/SubRunID.cc
+++
@@ -1,8 +0,0 @@
-#include "art/Persistency/Provenance/SubRunID.h"
-
-art::SubRunNumber_t const art::SubRunID::INVALID_SUBRUN_NUMBER = -1;
-art::SubRunNumber_t const art::SubRunID::MAX_VALID_SUBRUN_NUMBER = -2;
-art::SubRunNumber_t const art::SubRunID::FIRST_SUBRUN_NUMBER = 0;
-// art::SubRunNumber_t const art::SubRunID::INVALID_SUBRUN_NUMBER = 0;
-// art::SubRunNumber_t const art::SubRunID::MAX_VALID_SUBRUN_NUMBER = -1;
-// art::SubRunNumber_t const art::SubRunID::FIRST_SUBRUN_NUMBER = 1;
