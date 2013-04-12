5a5be8ff8376cdadc185e58b14b0d7c2bf06b3dc
Chris Green <artists@fnal.gov>
2013-04-12 07:25:57 -0500

    Paul's improvements to support loading of a ProductList into the appropriate registries.

--- art/Framework/Core/ProductRegistryHelper.cc
+++ art/Framework/Core/ProductRegistryHelper.cc
@@ -1,12 +1,17 @@
 #include "art/Framework/Core/ProductRegistryHelper.h"
 
 #include "art/Persistency/Provenance/BranchDescription.h"
+#include "art/Persistency/Provenance/BranchIDList.h"
+#include "art/Persistency/Provenance/BranchIDListHelper.h"
 #include "art/Persistency/Provenance/MasterProductRegistry.h"
 #include "art/Persistency/Provenance/ModuleDescription.h"
 #include "art/Persistency/Provenance/TypeLabel.h"
+#include "art/Utilities/DebugMacros.h"
 #include "art/Utilities/Exception.h"
 #include "cetlib/container_algorithms.h"
 #include "cpp0x/functional"
+
+#include <string>
 
 namespace {
   void
@@ -24,6 +29,34 @@ art::ProductRegistryHelper::
 registerProducts(MasterProductRegistry& preg,
                  ModuleDescription const& md)
 {
+  if (productList_) {
+    FDEBUG(1) << "ProductRegistryHelper::registerProducts: "
+              "merging product list ...\n";
+    preg.updateFromInput(*productList_.get());
+    FDEBUG(1) << "ProductRegistryHelper::registerProducts: "
+              "product list merged.\n";
+    BranchIDList bil;
+    for (auto I = productList_->begin(), E = productList_->end();
+            I != E; ++I) {
+        bil.push_back(I->second.branchID().id());
+    }
+    const std::string meAsAFilename("ProductRegistryHelper");
+    BranchIDLists bidlists;
+    bidlists.push_back(bil);
+    BranchIDListHelper::updateFromInput(bidlists, meAsAFilename);
+    if (art::debugit() >= 2) {
+      for (art::ProductList::const_iterator I = preg.productList().begin(),
+           E = preg.productList().end(); I != E; ++I) {
+        FDEBUG(2) << "ProductRegistryHelper::registerProducts: "
+                  << I->first << '\n';
+        FDEBUG(2) << "ProductRegistryHelper::registerProducts: present: "
+                  << I->second.present() << '\n';
+        FDEBUG(2) << "ProductRegistryHelper::registerProducts: produced: "
+                  << I->second.produced() << '\n';
+      }
+    }
+    productList_.reset();
+  }
   cet::for_all(typeLabelList_,
                std::bind(&addToRegistry,
                          std::placeholders::_1,
