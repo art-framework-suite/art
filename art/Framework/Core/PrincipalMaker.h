0a70bfa79d37d7e21d3984ee714cd70ff396c2d6
Paul Russo <russo@fnal.gov>
2013-04-25 15:18:54 -0500

    Add interfaces allowing creation of run and subRun principals with a specified auxiliary.
    Add interface allowing creation of an EventPrincipal with
    a specified auxiliary and a specified history.

--- art/Framework/Core/PrincipalMaker.h
+++ art/Framework/Core/PrincipalMaker.h
@@ -11,16 +11,23 @@
 #include "art/Framework/Principal/fwd.h"
 #include "art/Persistency/Provenance/EventAuxiliary.h"
 #include "art/Persistency/Provenance/EventID.h"
+#include "art/Persistency/Provenance/History.h"
 #include "art/Persistency/Provenance/RunID.h"
 #include "art/Persistency/Provenance/SubRunID.h"
 
+#include <memory>
+
 namespace art {
   class ProcessConfiguration;
+  class RunAuxiliary;
+  class SubRunAuxiliary;
   class Timestamp;
 
   class PrincipalMaker {
   public:
     explicit PrincipalMaker(ProcessConfiguration const & pconf);
+
+    RunPrincipal * makeRunPrincipal(RunAuxiliary const & runAux) const;
 
     RunPrincipal * makeRunPrincipal(RunID r,
                                     Timestamp const & startTime) const;
@@ -28,12 +35,19 @@ namespace art {
     RunPrincipal * makeRunPrincipal(RunNumber_t r,
                                     Timestamp const & startTime) const;
 
+    SubRunPrincipal * makeSubRunPrincipal(
+                                       SubRunAuxiliary const & subRunAux) const;
+
     SubRunPrincipal * makeSubRunPrincipal(SubRunID const & sr,
                                           Timestamp const & startTime) const;
 
     SubRunPrincipal * makeSubRunPrincipal(RunNumber_t r,
                                           SubRunNumber_t sr,
                                           Timestamp const & startTime) const;
+
+    EventPrincipal * makeEventPrincipal(EventAuxiliary const & eventAux,
+                                        std::shared_ptr<History> && history
+                                       ) const;
 
     EventPrincipal * makeEventPrincipal(EventID const & e,
                                         Timestamp const & startTime,
