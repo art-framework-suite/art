72bca9c59edc701cf7dfc86a74b16b30a1df1108
Chris Green <artists@fnal.gov>
2013-06-24 11:09:59 -0500

    Fix header location.

--- art/Framework/Core/PathsInfo.h
+++ art/Framework/Core/PathsInfo.h
@@ -1,7 +1,7 @@
 #ifndef art_Framework_Core_PathsInfo_h
 #define art_Framework_Core_PathsInfo_h
 #include "art/Framework/Core/Path.h"
-#include "art/Framework/Core/detail/WorkerMap.h"
+#include "art/Framework/Core/WorkerMap.h"
 #include "art/Persistency/Common/HLTGlobalStatus.h"
 
 namespace art {
