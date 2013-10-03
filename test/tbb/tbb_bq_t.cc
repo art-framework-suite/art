9da45838112006a0b5212cd3dbde4748bdff5473
Chris Green <greenc@fnal.gov>
2013-10-03 11:21:52 -0500

    Quick demo of concurrent_bounded_queue to check initialization.

---
+++ test/tbb/tbb_bq_t.cc
@@ -0,0 +1,22 @@
+#include "tbb/concurrent_queue.h"
+#include "tbb/task_scheduler_init.h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  tbb::task_scheduler_init theTBB;
+
+  std::vector<int> initVals(10);
+  std::iota(initVals.begin(), initVals.end(), 0);
+  tbb::concurrent_bounded_queue<int> bq(initVals.cbegin(), initVals.cend());
+  int i;
+  while (!bq.empty())
+  {
+    bq.pop(i);
+    std::cout << i << std::endl;
+    bq.push(i);
+  }
+}
