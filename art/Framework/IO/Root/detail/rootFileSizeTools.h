1981719002131adda5e5a6ae007135e66c2fce67
Kyle Knoepfel <knoepfel@fnal.gov>
2015-03-03 08:49:00 -0600

    Implement feature #5796

---
+++ art/Framework/IO/Root/detail/rootFileSizeTools.h
@@ -0,0 +1,29 @@
+#ifndef art_Framework_IO_Root_rootFileSizeTools_h
+#define art_Framework_IO_Root_rootFileSizeTools_h
+//
+// Utilties for finding the size on disk of TTrees and TBranches
+// Adapted from code given by Philippe Canal, pcanal@fnal.gov.
+//
+// This is known to work for root 5.34.*
+
+#include "TBranch.h"
+#include "TObjArray.h"
+#include "TTree.h"
+
+namespace art {
+  namespace detail {
+
+    Long64_t GetBasketSize(TObjArray * branches, bool ondisk, bool inclusive);
+    Long64_t GetBasketSize(TBranch * b, bool ondisk, bool inclusive);
+    Long64_t GetTotalSize( TBranch * br, bool ondisk, bool inclusive );
+    Long64_t GetTotalSize( TObjArray * branches, bool ondisk );
+    Long64_t GetTotalSize(TTree *t, bool ondisk);
+    Long64_t sizeOnDisk(TTree *t);
+    Long64_t sizeOnDisk(TBranch *branch, bool inclusive);
+    void     printBranchSummary(TBranch *br);
+    void     printTreeSummary(TTree *t);
+
+  }
+} // namespace art
+
+#endif /* art_Framework_IO_Root_rootFileSizeTools_h */
