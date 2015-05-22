72818934559d093a13a5d0f233cb1cd488456742
Chris Green <artists@fnal.gov>
2015-05-22 14:44:11 -0500

    Necessary fixes from compile-and-test.

--- art/Framework/Core/ProductRegistryHelper.cc
+++ art/Framework/Core/ProductRegistryHelper.cc
@@ -4,6 +4,7 @@
 #include "art/Persistency/Provenance/BranchDescription.h"
 #include "art/Persistency/Provenance/BranchIDList.h"
 #include "art/Persistency/Provenance/BranchIDListHelper.h"
+#include "art/Framework/Core/FileBlock.h"
 #include "art/Persistency/Provenance/MasterProductRegistry.h"
 #include "art/Persistency/Provenance/ModuleDescription.h"
 #include "art/Persistency/Provenance/TypeLabel.h"
@@ -18,18 +19,17 @@ art::ProductRegistryHelper::
 registerProducts(MasterProductRegistry& mpr,
                  ModuleDescription const& md)
 {
-  auto pl = std::move(productList_);
-  if (pl) {
+  if (productList_) {
     FileBlock fb({}, "ProductRegistryHelper");
     mpr.initFromFirstPrimaryFile(*productList_.get(), fb);
     BranchIDList bil;
-    for (auto const& val : productList_) {
+    for (auto const& val : *productList_.get()) {
         bil.push_back(val.second.branchID().id());
     }
     BranchIDListHelper::updateFromInput({bil}, fb.fileName());
   }
   for (auto const& val : typeLabelList_) {
-    mpr.addProduct(make_unique<art::BranchDescription>(val, md));
+    mpr.addProduct(std::make_unique<art::BranchDescription>(val, md));
   }
 }
 
