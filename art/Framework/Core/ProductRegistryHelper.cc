ee4809da0b577dae268ac73f764ae6646427ec3e
Chris Green <greenc@fnal.gov>
2015-07-21 15:43:25 -0500

    Fix issue #9672

--- art/Framework/Core/ProductRegistryHelper.cc
+++ art/Framework/Core/ProductRegistryHelper.cc
@@ -12,11 +12,8 @@
 #include "art/Utilities/Exception.h"
 #include "cetlib/container_algorithms.h"
 
+#include <cassert>
 #include <string>
-
-namespace {
-  using PerBranchTypePresence = art::MasterProductRegistry::PerBranchTypePresence;
-}
 
 void
 art::ProductRegistryHelper::
@@ -24,38 +21,20 @@ registerProducts(MasterProductRegistry& mpr,
                  ModuleDescription const& md)
 {
   if (productList_) {
-    FileBlock fb({}, "ProductRegistryHelper");
-    mpr.initFromFirstPrimaryFile(*productList_.get(),
-                                 perBranchTypePresence_(md),
-                                 fb);
     BranchIDList bil;
-    for (auto const& val : *productList_.get()) {
-        bil.push_back(val.second.branchID().id());
+    MasterProductRegistry::PerBranchTypePresence tp;
+    for (auto const& val : *productList_) {
+      auto const& bd = val.second;
+      auto bid = bd.branchID().id();
+      tp[bd.branchType()].emplace(bid);
+      bil.push_back(bid);
     }
+    FileBlock fb({}, "ProductRegistryHelper");
+    mpr.initFromFirstPrimaryFile(*productList_, tp, fb);
     BranchIDListHelper::updateFromInput({bil}, fb.fileName());
-    productList_.reset(); // No longer needed.
+    productList_.reset(); // Reset, since we no longer need it.
   }
   for (auto const& val : typeLabelList_) {
     mpr.addProduct(std::make_unique<art::BranchDescription>(val, md));
   }
-}
-
-PerBranchTypePresence
-art::ProductRegistryHelper::
-perBranchTypePresence_(ModuleDescription const &md)
-{
-  PerBranchTypePresence result;
-  for (auto const& tl : typeLabelList_) {
-    std::string branchName;
-    branchName += tl.friendlyClassName();
-    branchName += "_";
-    branchName += md.moduleLabel();
-    branchName += "_";
-    branchName += tl.productInstanceName;
-    branchName += "_";
-    branchName += md.processName();
-    branchName += ".";
-    result[tl.branchType].emplace( BranchID( branchName ) );
-  }
-  return result;
 }
