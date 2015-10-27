50880bf0769966c8f75b0dd3f6cf1f21ea875180
Chris Green <artists@fnal.gov>
2015-10-27 09:48:13 -0500

    Update checkClassVersion to be a separate target.

--- Modules/CheckClassVersion.cmake
+++ Modules/CheckClassVersion.cmake
@@ -1,4 +1,5 @@
 INCLUDE(CetParseArgs)
+INCLUDE(CheckUpsVersion)
 
 EXECUTE_PROCESS(COMMAND root-config --has-python
   RESULT_VARIABLE ART_CCV_ROOT_CONFIG_OK
@@ -15,6 +16,18 @@ IF(NOT ART_CCV_ENABLED)
     "with python support: ClassVersion checking is disabled."
     )
 ENDIF()
+
+FUNCTION(_check_prereqs VAR)
+  check_ups_version(cetbuildtools ${CETBUILDTOOLS_VERSION} v4_17_00 PRODUCT_MATCHES_VAR HAVE_BD_ALLDICTS)
+  SET(${VAR} ${HAVE_BD_ALLDICTS} PARENT_SCOPE)
+  IF (NOT HAVE_BD_ALLDICTS)
+    IF (HAVE_ROOT6)
+      message(FATAL_ERROR "check_class_version: proper operation with ROOT6 requires cetbuildtools >= 4.17.00")
+    ELSE (HAVE_ROOT6) # ROOT5
+      message(WARNING "check_class_version: use of cetbuildtools >= 4.17.00 is recommended with ROOT6")
+    ENDIF (HAVE_ROOT6)
+  ENDIF (NOT HAVE_BD_ALLDICTS)
+ENDFUNCTION()
 
 MACRO(check_class_version)
   CET_PARSE_ARGS(ART_CCV
@@ -33,15 +46,23 @@ MACRO(check_class_version)
     MESSAGE(FATAL_ERROR "CHECK_CLASS_VERSION must be called after BUILD_DICTIONARY.")
   ENDIF()
   IF(ART_CCV_ENABLED)
+    _check_prereqs(HAVE_BD_ALLDICTS)
     # Add the check to the end of the dictionary building step.
-    add_custom_command(TARGET ${dictname}_dict POST_BUILD
+    add_custom_command(OUTPUT ${dictname}_checked
       COMMAND checkClassVersion ${ART_CCV_EXTRA_ARGS}
       -l ${LIBRARY_OUTPUT_PATH}/lib${dictname}_dict
       -x ${CMAKE_CURRENT_SOURCE_DIR}/classes_def.xml
+      -t ${dictname}_checked
+      COMMENT "Checking class versions for ROOT dictionary ${dictname}"
+      DEPENDS ${LIBRARY_OUTPUT_PATH}/lib${dictname}_dict.so
       )
-    if (NOT ART_FRAMEWORK_CORE) # Building art.
-      add_dependencies(${dictname}_dict checkClassVersion)
-    endif()
+    add_custom_target(checkClassVersion_${dictname} ALL
+      DEPENDS ${dictname}_checked)
+    IF (HAVE_BD_ALLDICTS)
+      # All checkClassVersion invocations must wait until after *all*
+      # dictionaries have been built.
+      add_dependencies(checkClassVersion_${dictname} BuildDictionary_AllDicts)
+    ENDIF()
     if (ART_CCV_REQUIRED_DICTIONARIES)
       add_dependencies(${dictname}_dict ${ART_CCV_REQUIRED_DICTIONARIES})
     endif()
