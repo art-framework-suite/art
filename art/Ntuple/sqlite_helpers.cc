1ede0336ef5f5720990181a9f91d94861c9cf952
Kyle Knoepfel <knoepfel@fnal.gov>
2016-01-07 08:48:33 -0600

    Further tweaks to issue #11340 fix: forbid user-specified URI

--- art/Ntuple/sqlite_helpers.cc
+++ art/Ntuple/sqlite_helpers.cc
@@ -8,12 +8,11 @@
 #include <cmath>
 
 #include "art/Ntuple/sqlite_helpers.h"
+#include "art/Utilities/Exception.h"
 
-namespace sqlite
-{
+namespace sqlite {
 
-  namespace detail
-  {
+  namespace detail {
 
     //=======================================================================
     int getResult(void* data, int ncols [[gnu::unused]], char** results, char** /*cnames*/)
@@ -33,21 +32,21 @@ namespace sqlite
     {
       query_result res;
       char* errmsg = nullptr;
-      if ( sqlite3_exec(db, ddl.c_str(), detail::getResult, &res, &errmsg) != SQLITE_OK )
-        {
-          std::string msg(errmsg);
-          sqlite3_free(errmsg);
-          throw art::Exception(art::errors::SQLExecutionError, msg);
-        }
+      if ( sqlite3_exec(db, ddl.c_str(), detail::getResult, &res, &errmsg) != SQLITE_OK ) {
+        std::string msg{errmsg};
+        sqlite3_free(errmsg);
+        throw art::Exception(art::errors::SQLExecutionError) << msg;
+      }
       return res;
     }
 
-    //=========================================================================
-    /// hasTable(db, name, cnames) returns true if the db has a
-    /// table named 'name', with colums named 'cns' suitable for carrying a
-    /// tuple<ARGS...>. It returns false if there is no table of that name, and
-    /// throws an exception if there is a table of the given name but it
-    /// does not match both the given column names and column types.
+    //=================================================================
+    // hasTable(db, name, cnames) returns true if the db has a table
+    // named 'name', with colums named 'cns' suitable for carrying a
+    // tuple<ARGS...>. It returns false if there is no table of that
+    // name, and throws an exception if there is a table of the given
+    // name but it does not match both the given column names and
+    // column types.
     bool hasTable(sqlite3* db, std::string const& name, std::string const& sqlddl)
     {
       std::string cmd("select sql from sqlite_master where type=\"table\" and name=\"");
@@ -58,16 +57,28 @@ namespace sqlite
 
       if (res.data.size() == 0) { return false; }
       if (res.data.size() == 1 && res.data[0][0] == sqlddl) { return true; }
-      throw art::Exception(art::errors::SQLExecutionError,
-                           "Existing database table name does not match description");
+      throw art::Exception(art::errors::SQLExecutionError)
+        << "Existing database table name does not match description";
     }
 
+    //================================================================
+    // The locking mechanisms for nfs systems are deficient and can
+    // thus wreak havoc with sqlite, which depends upon them.  In
+    // order to support an sqlite database on nfs, we use a URI,
+    // explicitly including the query parameter: 'nolock=1'.  We will
+    // have to revisit this choice once we consider multiple
+    // processes/threads writing to the same database file.
     inline std::string assembleURI(std::string const& filename)
     {
-      return
-        filename.substr(0,5) == "file:" ?
-        filename : // if prefaced with 'file:', then it's a URI and just use it
-        "file:"+filename+"?nolock=1"; // otherwise append/prepend the necessary bits.
+      // Arbitrary decision: don't allow users to specify a URI since
+      // they may (unintentionally) remove the 'nolock' parameter,
+      // thus potentially causing issues with nfs.
+      if (filename.substr(0,5) == "file:") {
+        throw art::Exception(art::errors::Configuration)
+          << "art does not allow an SQLite database filename that starts with 'file:'.\n"
+          << "Please contact artists@fnal.gov if you believe this is an error.";
+      }
+      return "file:"+filename+"?nolock=1";
     }
 
   } // namespace detail
@@ -94,12 +105,11 @@ namespace sqlite
   void exec(sqlite3* db,std::string const& ddl)
   {
     char* errmsg = nullptr;
-    if ( sqlite3_exec(db, ddl.c_str(), nullptr, nullptr, &errmsg) != SQLITE_OK )
-      {
-        std::string msg(errmsg);
-        sqlite3_free(errmsg);
-        throw art::Exception(art::errors::SQLExecutionError, msg);
-      }
+    if ( sqlite3_exec(db, ddl.c_str(), nullptr, nullptr, &errmsg) != SQLITE_OK ) {
+      std::string msg{errmsg};
+      sqlite3_free(errmsg);
+      throw art::Exception(art::errors::SQLExecutionError) << msg;
+    }
   }
 
   //=======================================================================
@@ -117,28 +127,28 @@ namespace sqlite
   // Statistics helpers
 
   double mean( sqlite3* db, const std::string& tname, const std::string& colname ){
-    return query_db<double>( db, "select avg("s+colname+") from "s + tname );
+    return query_db<double>( db, "select avg("s+colname+") from " + tname );
   }
 
   double median( sqlite3* db, const std::string& tname, const std::string& colname ){
     return query_db<double>( db,
-                             "select avg("s+colname+")"s+
-                             " from (select "s+colname+
-                             " from "s+tname+
-                             " order by "s+colname+
-                             " limit 2 - (select count(*) from "s + tname+") % 2"s+
-                             " offset (select (count(*) - 1) / 2"s+
-                             " from "s + tname+"))"s );
+                             "select avg("s+colname+")"+
+                             " from (select "+colname+
+                             " from "+tname+
+                             " order by "+colname+
+                             " limit 2 - (select count(*) from " + tname+") % 2"+
+                             " offset (select (count(*) - 1) / 2"+
+                             " from " + tname+"))" );
   }
 
   double rms( sqlite3* db, const std::string& tname, const std::string& colname ){
     double const ms = query_db<double>( db,
                                         "select sum("s+
-                                        "("s + colname + "-(select avg("s + colname + ") from "s + tname +"))"s +
-                                        "*"s +
-                                        "("s + colname + "-(select avg("s + colname + ") from "s + tname +"))"s +
-                                        " ) /"s +
-                                        "(count("s + colname +")) from "s + tname );
+                                        "(" + colname + "-(select avg(" + colname + ") from " + tname +"))" +
+                                        "*" +
+                                        "(" + colname + "-(select avg(" + colname + ") from " + tname +"))" +
+                                        " ) /" +
+                                        "(count(" + colname +")) from " + tname );
     return std::sqrt( ms );
   }
 
