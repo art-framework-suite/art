eb7f8833682e8ea7719421bec5a887f71d858ed4
Paul Russo <russo@fnal.gov>
2016-08-17 15:44:27 -0500

    Switch to entry-by-entry copying of the input data if fast cloning fails.
    This implements feature request #13275.

---
+++ art/test/Integration/fastclonefail/v11/ClonedProd.cc
@@ -0,0 +1,17 @@
+#include "art/test/Integration/fastclonefail/v11/ClonedProd.h"
+
+namespace arttest {
+
+ClonedProd::
+~ClonedProd()
+{
+}
+
+ClonedProd::
+ClonedProd()
+  : dp1_(5.0)
+{
+}
+
+} // namespace arttest
+
---
+++ art/test/Integration/fastclonefail/v11/test_fastclonefail_r.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. cet_test_functions.sh
+
+rm -f copy.root
+
+export LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=.:${DYLD_LIBRARY_PATH}
+
+art -c "${1}" -s "${2}" --rethrow-all
+
+check_files "copy.root"
+
