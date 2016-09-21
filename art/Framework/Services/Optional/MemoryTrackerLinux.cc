b01528bbf2c63d9a246d25e60ab7946af15ef6e4
Kyle Knoepfel <knoepfel@fnal.gov>
2016-09-21 11:46:53 -0500

    Fix bug #13873: remove 'ignoreTotal' feature, which was incorrectly implemented.
    Option replaced with 'maxTableLength' configuration parameter, which
    can limit the number of rows printed in the summary tables.

--- art/Framework/Services/Optional/MemoryTrackerLinux.cc
+++ art/Framework/Services/Optional/MemoryTrackerLinux.cc
@@ -139,8 +139,8 @@ using namespace aliases;
 
 art::MemoryTracker::MemoryTracker(ServiceTable<Config> const& config,
                                   ActivityRegistry& iReg)
-  : numToSkip_     {config().ignoreTotal()}
-  , printSummary_  {setbits_(config().printSummaries())}
+  : printSummary_  {setbits_(config().printSummaries())}
+  , maxTableLength_{config().maxTableLength()}
   , dbMgr_         {config().dbOutput().filename()}
   , overwriteContents_{config().dbOutput().overwrite()}
   , includeMallocInfo_{checkMallocConfig_(config().dbOutput().filename(),
@@ -161,6 +161,11 @@ art::MemoryTracker::MemoryTracker(ServiceTable<Config> const& config,
   , modBeginSubRun_ {otherInfoTable_, procInfo_, "Module beginSubRun"}
   , modEndSubRun_   {otherInfoTable_, procInfo_, "Module endSubRun"}
 {
+  unsigned i{};
+  if (config().ignoreTotal(i)) {
+    mf::LogWarning("MemoryTracker") << "The 'services.MemoryTracker.ignoreTotal' parameter is deprecated;\n"
+                                    << "its value of " << i << " will not be used.  Contact artists@fnal.gov for questions.";
+  }
   iReg.sPreModuleConstruction .watch( &this->modConstruction_, &CallbackPair::pre  );
   iReg.sPostModuleConstruction.watch( &this->modConstruction_, &CallbackPair::post );
   iReg.sPreModuleBeginJob     .watch( &this->modBeginJob_    , &CallbackPair::pre  );
@@ -275,7 +280,7 @@ art::MemoryTracker::postEndJob()
   if (printSummary_.test(GENERAL)) {
     msgOss << rule << "\n\n";
     msgOss << std::left << "MemoryTracker General SUMMARY (all numbers in units of Mbytes)\n\n";
-    generalSummary_( msgOss );
+    generalSummary_(msgOss);
   }
   if (printSummary_.test(EVENT)) {
     msgOss << rule << "\n\n";
@@ -440,14 +445,12 @@ art::MemoryTracker::eventSummary_(std::ostringstream& oss,
   eventTable_.flush();
 
   eventDataList_t evtList;
-  std::size_t i{};
   using namespace sqlite;
 
   result r;
-  r << select("*").from(eventTable_).where(column+" > 0").order_by(column,"desc").limit(5);
+  r << select("*").from(eventTable_).where(column+" > 0").order_by(column,"desc").limit(maxTableLength_);
 
   for (auto& entry : r) {
-    if (i++ < numToSkip_) continue;
     auto evtData = convertToEvtIdData(entry);
     evtList.push_back(std::move(evtData));
   }
@@ -464,7 +467,7 @@ art::MemoryTracker::eventSummary_(std::ostringstream& oss,
       << rule << "\n";
 
   if (evtList.empty()) {
-    oss << "  [[ no events ]] " << "\n";
+    oss << "  [ no events ]\n";
   }
 
   for (auto const& data : evtList) {
@@ -513,12 +516,9 @@ art::MemoryTracker::moduleSummary_(std::ostringstream& oss,
     std::string const& columns = "Run,Subrun,Event,Vsize,DeltaVsize,RSS,DeltaRSS";
 
     eventDataList_t evtList;
-    std::size_t i{};
-
     for (auto& entry : query(dbMgr_,
                              "SELECT "s+columns+" FROM temp.tmpModTable "s+
-                             "WHERE "+column+" > 0 ORDER BY "s+column+" DESC LIMIT 5"s)) {
-      if (i++ < numToSkip_) continue;
+                             "WHERE "+column+" > 0 ORDER BY "s+column+" DESC LIMIT "s+std::to_string(maxTableLength_))) {
       auto evtData = convertToEvtIdData(entry);
       evtList.push_back(std::move(evtData));
     }
@@ -546,7 +546,7 @@ art::MemoryTracker::moduleSummary_(std::ostringstream& oss,
     oss << itMod->first << "\n";
 
     if (itMod->second.empty()) {
-      oss << "  [[ no events ]] " << "\n";
+      oss << "  [ no events ]\n";
     }
 
     for (auto const& evtInfo : itMod->second) {
