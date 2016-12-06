197535fb50931e5a14cdc5f8ca93d9c8da2045d8
Chris Green <artists@fnal.gov>
2016-12-06 17:34:49 -0600

    Remove unnecessary condition nesting.

--- art/Framework/Principal/GroupFactory.cc
+++ art/Framework/Principal/GroupFactory.cc
@@ -33,31 +33,30 @@ getWrapperTIDs(BranchDescription const& bd)
   TypeWithDict const tw(twName);
   maybeThrowLateDictionaryError(tw, twName);
   result.emplace_back(tw.id()); // Wrapper.
-  if (ta.category() == TypeWithDict::Category::CLASSTYPE) {
-    if (is_instantiation_of(ta.tClass(), "art::Assns")) {
-      auto const tpName = name_of_assns_partner(taName);
-      TypeWithDict const tp(tpName);
-      maybeThrowLateDictionaryError(tp, tpName);
-      auto const twpName = art::wrappedClassName(tpName);
-      TypeWithDict const twp(twpName);
-      maybeThrowLateDictionaryError(twp, twpName);
-      result.emplace_back(twp.id()); // Wrapper of partner.
-      auto const baseName = name_of_assns_base(taName);
-      if (!baseName.empty()) {
-        TypeWithDict const base(baseName);
-        maybeThrowLateDictionaryError(base, baseName);
-        auto const basewName = art::wrappedClassName(baseName);
-        TypeWithDict const basew(basewName);
-        maybeThrowLateDictionaryError(basew, basewName);
-        result.emplace_back(basew.id()); // Wrapper of base.
-        auto const basepName = name_of_assns_partner(baseName);
-        TypeWithDict const basep(basepName);
-        maybeThrowLateDictionaryError(basep, basepName);
-        auto const basewpName = art::wrappedClassName(basepName);
-        TypeWithDict const basewp(basewpName);
-        maybeThrowLateDictionaryError(basewp, basewpName);
-        result.emplace_back(basewp.id()); // Wrapper of partner's base.
-      }
+  if (ta.category() == TypeWithDict::Category::CLASSTYPE &&
+      is_instantiation_of(ta.tClass(), "art::Assns")) {
+    auto const tpName = name_of_assns_partner(taName);
+    TypeWithDict const tp(tpName);
+    maybeThrowLateDictionaryError(tp, tpName);
+    auto const twpName = art::wrappedClassName(tpName);
+    TypeWithDict const twp(twpName);
+    maybeThrowLateDictionaryError(twp, twpName);
+    result.emplace_back(twp.id()); // Wrapper of partner.
+    auto const baseName = name_of_assns_base(taName);
+    if (!baseName.empty()) {
+      TypeWithDict const base(baseName);
+      maybeThrowLateDictionaryError(base, baseName);
+      auto const basewName = art::wrappedClassName(baseName);
+      TypeWithDict const basew(basewName);
+      maybeThrowLateDictionaryError(basew, basewName);
+      result.emplace_back(basew.id()); // Wrapper of base.
+      auto const basepName = name_of_assns_partner(baseName);
+      TypeWithDict const basep(basepName);
+      maybeThrowLateDictionaryError(basep, basepName);
+      auto const basewpName = art::wrappedClassName(basepName);
+      TypeWithDict const basewp(basewpName);
+      maybeThrowLateDictionaryError(basewp, basewpName);
+      result.emplace_back(basewp.id()); // Wrapper of partner's base.
     }
   }
   return result;
