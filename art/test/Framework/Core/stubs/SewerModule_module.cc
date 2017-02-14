9974f290f6d8c3fc8003227fa333d517024061c9
Kyle Knoepfel <knoepfel@fnal.gov>
2017-02-14 11:20:34 -0600

    Remove obsolete stubs directories.

--- art/test/Framework/Core/stubs/SewerModule_module.cc
+++
@@ -1,6 +0,0 @@
-
-extern "C"
-{
-  extern int a_trick_for_test_filters;
-}
-
--- art/test/Framework/Core/stubs/TestBeginEndJobAnalyzerModule.cc
+++
@@ -1,5 +0,0 @@
-#include "art/Framework/Core/ModuleMacros.h"
-#include "FWCore/Framework/test/stubs/TestBeginEndJobAnalyzer.h"
-
-//define this as a plug-in
-DEFINE_ART_MODULE(TestBeginEndJobAnalyzer)
