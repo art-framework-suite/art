916fe5d4f7e68118e4859d27c26295d7ae32b3f3
Kyle Knoepfel <knoepfel@fnal.gov>
2017-03-17 12:22:32 -0500

    Refinements to TKeyVFSOpenPolicy.

--- art/Persistency/RootDB/TKeyVFSOpenPolicy.h
+++ art/Persistency/RootDB/TKeyVFSOpenPolicy.h
@@ -1,12 +1,10 @@
 #ifndef art_Persistency_RootDB_TKeyVFSOpenPolicy_h
 #define art_Persistency_RootDB_TKeyVFSOpenPolicy_h
 
-#include "art/Persistency/RootDB/tkeyvfs.h"
-#include "canvas/Utilities/Exception.h"
-
 #include "TFile.h"
-
 #include "sqlite3.h"
+
+#include <string>
 
 namespace art {
 
@@ -14,31 +12,9 @@ namespace art {
   public:
 
     explicit TKeyVFSOpenPolicy(TFile* const tfile,
-                               int const flags = SQLITE_OPEN_READONLY)
-      : tfile_{tfile}
-      , flags_{flags}
-    {}
+                               int const flags = SQLITE_OPEN_READONLY);
 
-    sqlite3* open(std::string const& key)
-    {
-      if (!key.size()) {
-        throw Exception{errors::FileOpenError}
-        << "Failed to open TKEYVFS DB due to empty key spec.\n";
-      }
-      sqlite3* db {nullptr};
-      int const rc {tkeyvfs_open_v2(key.c_str(), &db, flags_, tfile_)};
-      if (rc != SQLITE_OK) {
-        throw Exception{errors::FileOpenError}
-        << "Failed to open requested DB, \""
-             << key
-             << "\" of type, \""
-             << "tkeyvfs"
-             << "\" -- "
-             << sqlite3_errmsg(db)
-             << "\n";
-      }
-      return db;
-    }
+    sqlite3* open(std::string const& key);
 
   private:
     TFile* tfile_;
