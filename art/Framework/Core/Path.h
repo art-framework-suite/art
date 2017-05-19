art-framework-suite/art

commit e75def7b53e3c4ef7e114940a77449d3eec6c45a

Resolve issue #7731: retain only job-level stopwatch.

art/Framework/Core/Path.h
@@ -11,7 +11,6 @@
 
 #include "art/Framework/Principal/CurrentProcessingContext.h"
 #include "art/Framework/Principal/PrincipalPackages.h"
-#include "art/Framework/Principal/MaybeRunStopwatch.h"
 #include "art/Framework/Principal/Worker.h"
 #include "art/Framework/Core/WorkerInPath.h"
 #include "canvas/Persistency/Common/HLTenums.h"
@@ -52,11 +51,6 @@ public:
   int bitPosition() const { return bitpos_; }
   std::string const& name() const { return name_; }
 
-  std::pair<double,double> timeCpuReal() const
-  {
-    return std::pair<double,double>(stopwatch_.cpuTime(), stopwatch_.realTime());
-  }
-
   void clearCounters();
 
   std::size_t timesRun() const { return timesRun_; }
@@ -74,7 +68,6 @@ private:
 
   void findByModifiesEvent(bool modifies, std::vector<std::string>& foundLabels) const;
 
-  Stopwatch::timer_type stopwatch_ {};
   std::size_t timesRun_ {};
   std::size_t timesPassed_ {};
   std::size_t timesFailed_ {};
@@ -102,36 +95,30 @@ private:
 template <typename T>
 void art::Path::process(typename T::MyPrincipal& ep)
 {
-  // Create the PathSignalSentry before the MaybeRunStopwatch so that
-  // we only record the time spent in the path not from the signal
-
   int nwrwue {-1}; // numWorkersRunWithoutUnhandledException
   T::prePathSignal(actReg_, name_);
-  {
-    MaybeRunStopwatch<T::level> sentry {stopwatch_};
 
-    if (T::level == Level::Event) {
-      ++timesRun_;
+  if (T::level == Level::Event) {
+    ++timesRun_;
+  }
+  state_ = hlt::Ready;
+
+  bool should_continue {true};
+  CurrentProcessingContext cpc {&name_, bitPosition(), isEndPath_};
+
+  for (auto it = workers_.begin(), end = workers_.end(); it != end && should_continue; ++it) {
+    ++nwrwue;
+    try {
+      cpc.activate(nwrwue, it->getWorker()->descPtr());
+      should_continue = it->runWorker<T>(ep, &cpc);
     }
-    state_ = hlt::Ready;
-
-    bool should_continue {true};
-    CurrentProcessingContext cpc {&name_, bitPosition(), isEndPath_};
-
-    for (auto it = workers_.begin(), end = workers_.end(); it != end && should_continue; ++it) {
-      ++nwrwue;
-      try {
-        cpc.activate(nwrwue, it->getWorker()->descPtr());
-        should_continue = it->runWorker<T>(ep, &cpc);
-      }
-      catch (cet::exception& e) {
-        // handleWorkerFailure may throw a new exception.
-        should_continue = handleWorkerFailure(e, nwrwue, T::level == Level::Event);
-      }
-      catch (...) {
-        recordUnknownException(nwrwue, T::level == Level::Event);
-        throw;
-      }
+    catch (cet::exception& e) {
+      // handleWorkerFailure may throw a new exception.
+      should_continue = handleWorkerFailure(e, nwrwue, T::level == Level::Event);
+    }
+    catch (...) {
+      recordUnknownException(nwrwue, T::level == Level::Event);
+      throw;
     }
     updateCounters(should_continue, T::level == Level::Event);
     recordStatus(nwrwue, T::level == Level::Event);

art/Framework/Core/EndPathExecutor.h
@@ -24,7 +24,6 @@
 #include "art/Framework/Principal/EventPrincipal.h"
 #include "art/Framework/Principal/PrincipalPackages.h"
 #include "art/Framework/Principal/RangeSetHandler.h"
-#include "art/Framework/Principal/MaybeRunStopwatch.h"
 #include "art/Framework/Principal/Worker.h"
 #include "canvas/Persistency/Provenance/BranchType.h"
 #include "cetlib/trim.h"
@@ -117,7 +116,7 @@ art::EndPathExecutor::
 process(typename T::MyPrincipal & ep)
 {
   this->resetAll();
-  auto sentry (endPathInfo_.maybeRunStopwatch<T::level>());
+
   if (T::level == Level::Event) {
     endPathInfo_.addEvent();
   }

art/Framework/Core/PathManager.cc
@@ -142,21 +142,19 @@ art::PathManager::PathManager(ParameterSet const& procPS,
 art::PathsInfo&
 art::PathManager::endPathInfo()
 {
-  if (!protoEndPathInfo_.empty()&&
-      endPathInfo_.pathPtrs().empty()) {
+  if (!protoEndPathInfo_.empty() && endPathInfo_.pathPtrs().empty()) {
     // Need to create path from proto information.
-    endPathInfo_.pathPtrs().emplace_back
-      (fillWorkers_(0,
-                    "end_path",
-                    protoEndPathInfo_,
-                    nullptr, // End path, no trigger results needed.
-                    endPathInfo_.workers()));
+    endPathInfo_.pathPtrs().emplace_back(fillWorkers_(0,
+                                                      "end_path",
+                                                      protoEndPathInfo_,
+                                                      nullptr, // End path, no trigger results needed.
+                                                      endPathInfo_.workers()));
   }
   return endPathInfo_;
 }
 
 art::PathsInfo&
-art::PathManager::triggerPathsInfo(ScheduleID sID)
+art::PathManager::triggerPathsInfo(ScheduleID const sID)
 {
   if (triggerPathNames_.empty())
     return triggerPathsInfo_[sID]; // Empty.
@@ -181,14 +179,13 @@ art::PathManager::triggerPathsInfo(ScheduleID sID)
 art::detail::ModuleConfigInfoMap
 art::PathManager::fillAllModules_()
 {
-  static ParameterSet const empty;
   detail::ModuleConfigInfoMap all_modules;
   std::ostringstream error_stream;
   for (auto const& pathRootName : detail::ModuleConfigInfo::allModulePathRoots()) {
-    auto const pathRoot = procPS_.get<ParameterSet>(pathRootName, empty);
+    auto const pathRoot = procPS_.get<ParameterSet>(pathRootName, {});
     for (auto const& name : pathRoot.get_names()) {
       try {
-        detail::ModuleConfigInfo mci(procPS_, name, pathRootName);
+        detail::ModuleConfigInfo const mci {procPS_, name, pathRootName};
         auto actualModType = fact_.moduleType(mci.libSpec());
         if (actualModType != mci.moduleType()) {
           error_stream
@@ -237,9 +234,7 @@ art::PathManager::vstring
 art::PathManager::processPathConfigs_()
 {
   vstring trigger_path_names;
-  auto services = procPS_.get<ParameterSet>("services",{});
-  auto opts(services.get<ParameterSet>("scheduler", ParameterSet()));
-  auto nSchedules [[gnu::unused]] = opts.get<size_t>("num_schedules", 1);
+  auto nSchedules [[gnu::unused]] = procPS_.get<size_t>("services.scheduler.num_schedules", 1);
   // Identify and process paths.
   std::set<std::string> known_pars {
     "analyzers",

art/Framework/Core/PathsInfo.h
@@ -1,8 +1,8 @@
 #ifndef art_Framework_Core_PathsInfo_h
 #define art_Framework_Core_PathsInfo_h
+
 #include "art/Framework/Core/Path.h"
 #include "art/Framework/Core/WorkerMap.h"
-#include "art/Framework/Principal/MaybeRunStopwatch.h"
 #include "canvas/Persistency/Common/HLTGlobalStatus.h"
 
 namespace art {
@@ -19,15 +19,11 @@ public:
   void addEvent();
   void addPass();
 
-  template <Level L>
-  MaybeRunStopwatch<L> maybeRunStopwatch() { return {stopwatch_};}
-
   WorkerMap const& workers() const;
   PathPtrs const& pathPtrs() const;
   size_t passedEvents() const;
   size_t failedEvents() const;
   size_t totalEvents() const;
-  std::pair<double, double> timeCpuReal() const;
 
 private:
   WorkerMap workers_ {};
@@ -36,95 +32,76 @@ private:
 
   size_t totalEvents_ {};
   size_t passedEvents_ {};
-  Stopwatch::timer_type stopwatch_ {};
 };
 
 inline
-art::WorkerMap &
-art::PathsInfo::
-workers()
+art::WorkerMap&
+art::PathsInfo::workers()
 {
   return workers_;
 }
 
 inline
-art::PathPtrs &
-art::PathsInfo::
-pathPtrs()
+art::PathPtrs&
+art::PathsInfo::pathPtrs()
 {
   return pathPtrs_;
 }
 
 inline
-art::HLTGlobalStatus &
-art::PathsInfo::
-pathResults()
+art::HLTGlobalStatus&
+art::PathsInfo::pathResults()
 {
   return pathResults_;
 }
 
 inline
 void
-art::PathsInfo::
-addEvent()
+art::PathsInfo::addEvent()
 {
   ++totalEvents_;
 }
 
 inline
 void
-art::PathsInfo::
-addPass()
+art::PathsInfo::addPass()
 {
   ++passedEvents_;
 }
 
 inline
-art::WorkerMap const &
-art::PathsInfo::
-workers() const
+art::WorkerMap const&
+art::PathsInfo::workers() const
 {
   return workers_;
 }
 
 inline
-art::PathPtrs const &
-art::PathsInfo::
-pathPtrs() const
+art::PathPtrs const&
+art::PathsInfo::pathPtrs() const
 {
   return pathPtrs_;
 }
 
 inline
 size_t
-art::PathsInfo::
-passedEvents() const
+art::PathsInfo::passedEvents() const
 {
   return passedEvents_;
 }
 
 inline
 size_t
-art::PathsInfo::
-failedEvents() const
+art::PathsInfo::failedEvents() const
 {
   return totalEvents_ - passedEvents_;
 }
 
 inline
 size_t
-art::PathsInfo::
-totalEvents() const
+art::PathsInfo::totalEvents() const
 {
   return totalEvents_;
-}
-
-inline
-std::pair<double, double>
-art::PathsInfo::
-timeCpuReal() const
-{
-  return std::pair<double, double>(stopwatch_.cpuTime(), stopwatch_.realTime());
 }
 
 #endif /* art_Framework_Core_PathsInfo_h */

art/Framework/Core/Schedule.cc
@@ -25,11 +25,8 @@
 #include "cetlib/exempt_ptr.h"
 
 #include <algorithm>
-#include <algorithm>
 #include <cassert>
 #include <cstdlib>
-#include <iomanip>
-#include <numeric>
 
 using fhicl::ParameterSet;
 
@@ -37,13 +34,12 @@ art::Schedule::
 Schedule(ScheduleID sID, PathManager& pm, ParameterSet const& proc_pset,
          TriggerNamesService const& tns, MasterProductRegistry& mpr,
          ActionTable& actions, ActivityRegistry& areg)
-  : sID_(sID)
-  , process_pset_(proc_pset)
-  , act_table_(&actions)
-  , processName_(tns.getProcessName())
-  , triggerPathsInfo_(pm.triggerPathsInfo(sID_))
+  : sID_{sID}
+  , process_pset_{proc_pset}
+  , act_table_{&actions}
+  , processName_{tns.getProcessName()}
+  , triggerPathsInfo_{pm.triggerPathsInfo(sID_)}
   , pathsEnabled_(triggerPathsInfo_.pathPtrs().size(), true)
-  , results_inserter_()
 {
   if (!triggerPathsInfo_.pathPtrs().empty()) {
     makeTriggerResultsInserter_(tns.getTriggerPSet(), mpr, areg);
@@ -132,12 +128,11 @@ beginJob()
 
 void
 art::Schedule::
-makeTriggerResultsInserter_(fhicl::ParameterSet const & trig_pset,
-                            MasterProductRegistry & mpr,
-                            ActivityRegistry & areg)
+makeTriggerResultsInserter_(fhicl::ParameterSet const& trig_pset,
+                            MasterProductRegistry& mpr,
+                            ActivityRegistry& areg)
 {
-  WorkerParams work_args(process_pset_, trig_pset, mpr, *act_table_,
-                         processName_);
+  WorkerParams const work_args{process_pset_, trig_pset, mpr, *act_table_, processName_};
   ModuleDescription md(trig_pset.id(),
                        "TriggerResultInserter",
                        "TriggerResults",
@@ -146,10 +141,8 @@ makeTriggerResultsInserter_(fhicl::ParameterSet const & trig_pset,
                                             getReleaseVersion(),
                                             getPassID()));
   areg.sPreModuleConstruction.invoke(md);
-  auto producer = std::make_unique<TriggerResultInserter>(trig_pset,
-                                                          triggerPathsInfo_.pathResults());
+  auto producer = std::make_unique<TriggerResultInserter>(trig_pset, triggerPathsInfo_.pathResults());
   results_inserter_ = std::make_unique<WorkerT<EDProducer>>(std::move(producer), md, work_args);
   areg.sPostModuleConstruction.invoke(md);
-  results_inserter_->setActivityRegistry(cet::exempt_ptr<ActivityRegistry>
-                                         (&areg));
+  results_inserter_->setActivityRegistry(cet::exempt_ptr<ActivityRegistry>(&areg));
 }

art/Framework/Core/Schedule.h
@@ -30,7 +30,6 @@
 #include "art/Framework/Principal/EventPrincipal.h"
 #include "art/Framework/Principal/PrincipalPackages.h"
 #include "art/Framework/Principal/Provenance.h"
-#include "art/Framework/Principal/MaybeRunStopwatch.h"
 #include "art/Framework/Principal/Worker.h"
 #include "art/Framework/Principal/fwd.h"
 #include "art/Framework/Services/Registry/ActivityRegistry.h"
@@ -117,7 +116,6 @@ Schedule::process(typename T::MyPrincipal& principal)
     w->reset();
   });
   triggerPathsInfo_.pathResults().reset();
-  MaybeRunStopwatch<T::level> sentry {triggerPathsInfo_.maybeRunStopwatch<T::level>()};
   if (T::level == Level::Event) {
     triggerPathsInfo_.addEvent();
   }

art/Framework/Core/WorkerInPath.h
@@ -10,7 +10,6 @@
 
 #include "art/Framework/Core/Frameworkfwd.h"
 #include "art/Framework/Principal/ExecutionCounts.h"
-#include "art/Framework/Principal/MaybeRunStopwatch.h"
 #include "art/Framework/Principal/Worker.h"
 
 #include <memory>
@@ -31,11 +30,6 @@ namespace art {
     bool runWorker(typename T::MyPrincipal&,
                    CurrentProcessingContext const* cpc);
 
-    std::pair<double,double> timeCpuReal() const
-    {
-      return std::pair<double,double>(stopwatch_.cpuTime(), stopwatch_.realTime());
-    }
-
     void clearCounters()
     {
       counts_ = Counts_t{};
@@ -53,7 +47,6 @@ namespace art {
     bool modifiesEvent() const { return worker_->modifiesEvent(); }
 
   private:
-    Stopwatch::timer_type stopwatch_ {};
 
     using Counts_t = ExecutionCounts<stats::Visited, stats::Passed, stats::Failed, stats::ExceptionThrown>;
     Counts_t counts_ {};
@@ -69,7 +62,6 @@ namespace art {
     MaybeIncrementCounts<T::level, decltype(counts_)> counts {counts_};
     counts.template increment<stats::Visited>();
 
-    MaybeRunStopwatch<T::level> sentry {stopwatch_};
     bool rc {true};
     try {
       // may want to change the return value from the worker to be the
@@ -77,7 +69,7 @@ namespace art {
       // to identify
       rc = worker_->doWork<T>(ep, cpc);
     }
-    catch(...) {
+    catch (...) {
       counts.template increment<stats::ExceptionThrown>();
       throw;
     }

art/Framework/EventProcessor/EventProcessor.cc
@@ -257,6 +257,7 @@ namespace art {
   template <>
   inline void EventProcessor::begin<Level::Job>()
   {
+    timer_.start();
     beginJob();
   }
 
@@ -348,6 +349,7 @@ namespace art {
   inline void EventProcessor::finalize<Level::Job>()
   {
     endJob();
+    timer_.stop();
   }
 
   template <>
@@ -530,7 +532,8 @@ art::EventProcessor::endJob()
   ec_.call([this]{ schedule_->endJob(); });
   ec_.call([this]{ endPathExecutor_->endJob(); });
   ec_.call([this]{ detail::writeSummary(pathManager_,
-                                        ServiceHandle<TriggerNamesService const>{}->wantSummary()); });
+                                        ServiceHandle<TriggerNamesService const>{}->wantSummary(),
+                                        timer_); });
   ec_.call([this]{ input_->doEndJob(); });
   ec_.call([this]{ actReg_.sPostEndJob.invoke(); });
 }

commit bcda0994c0e97962c494831e95b5e82e20f244a7

Fix bookkeeping error introduced in commit e75def7b.
Rename the TimeTracker test files to something more sensible.

art/Framework/Core/Path.h
@@ -120,9 +120,9 @@ void art::Path::process(typename T::MyPrincipal& ep)
       recordUnknownException(nwrwue, T::level == Level::Event);
       throw;
     }
-    updateCounters(should_continue, T::level == Level::Event);
-    recordStatus(nwrwue, T::level == Level::Event);
   }
+  updateCounters(should_continue, T::level == Level::Event);
+  recordStatus(nwrwue, T::level == Level::Event);
   HLTPathStatus const status(state_, nwrwue);
   T::postPathSignal(actReg_, name_, status);
 }

art/test/Integration/CMakeLists.txt
@@ -1040,21 +1040,25 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     )
 endif()
 
-cet_test(TimeTracker_issue_3598_t1 HANDBUILT
+cet_test(TimeTracker_t1 HANDBUILT
   TEST_EXEC art
-  TEST_ARGS -c issue_3598_t1.fcl
-  DATAFILES fcl/issue_3598_t1.fcl
-  REF "${CMAKE_CURRENT_SOURCE_DIR}/TimeTracker_issue_3598_t1-ref.txt"
+  TEST_ARGS -c TimeTracker_t1.fcl
+  DATAFILES fcl/TimeTracker_t1.fcl
+  REF "${CMAKE_CURRENT_SOURCE_DIR}/TimeTracker_t1-ref.txt"
   OUTPUT_FILTERS "filter-timeTracker-output" "DEFAULT"
   )
 
-cet_test(TimeTracker_issue_3598_t2 HANDBUILT TEST_EXEC art TEST_ARGS -c issue_3598_t2.fcl DATAFILES fcl/issue_3598_t2.fcl)
-
-cet_test(TimeTracker_issue_3598_t3 HANDBUILT
+cet_test(TimeTracker_t2 HANDBUILT
   TEST_EXEC art
-  TEST_ARGS -c issue_3598_t3.fcl
-  DATAFILES fcl/issue_3598_t3.fcl
-  REF "${CMAKE_CURRENT_SOURCE_DIR}/TimeTracker_issue_3598_t3-ref.txt"
+  TEST_ARGS -c TimeTracker_t2.fcl
+  DATAFILES fcl/TimeTracker_t2.fcl
+  )
+
+cet_test(TimeTracker_t3 HANDBUILT
+  TEST_EXEC art
+  TEST_ARGS -c TimeTracker_t3.fcl
+  DATAFILES fcl/TimeTracker_t3.fcl
+  REF "${CMAKE_CURRENT_SOURCE_DIR}/TimeTracker_t3-ref.txt"
   OUTPUT_FILTERS "filter-timeTracker-output" "DEFAULT"
   )
 