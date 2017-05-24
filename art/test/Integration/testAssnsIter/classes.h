0560afd368b0f99fdad28a7911e7a88b3811e2db
Saba Sehrish <ssehrish@fnal.gov>
2017-05-24 09:41:28 -0500

    Integration test for Assns<A,B,D> forward iterator.

---
+++ art/test/Integration/testAssnsIter/classes.h
@@ -0,0 +1,4 @@
+#include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/Wrapper.h"
