a1e99a4ad8dbd31cd6523453ff2160c8d01bc413
Kyle Knoepfel <knoepfel@fnal.gov>
2018-01-16 13:36:05 -0600

    Fix bug #18378: Add static member 'create' to PtrMaker.

--- art/Persistency/Common/PtrMaker.h
+++ art/Persistency/Common/PtrMaker.h
@@ -4,36 +4,45 @@
 //
 // Author:      Saba Sehrish
 //
-// Description: A common pattern is to create a collection A,
-// and a collection B, create art::Ptrs to the objects in each of
-// the collection and then create associations between the objects
-// in the two collections. The purpose of art::PtrMaker is to simplify
-// the process of creating art::Assns by providing a utility to create
+// Description: A common pattern is to create a collection A, and a
+// collection B, create art::Ptrs to the objects in each of the
+// collection and then create associations between the objects in the
+// two collections. The purpose of art::PtrMaker is to simplify the
+// process of creating art::Assns by providing a utility to create
 // art::Ptrs. It is a two step process to create an art::Ptr with this
 // approach.
-// Step I has two cases; the product and ptrs are constructed
-// in the same module, or in different modules. For each case, there
-// is a way to construct a PtrMaker object.
 //
-// case 1: Construct a PtrMaker object that creates Ptrs in to a collection
-// of type C, the most common one is std::vector and hence is the default,
-// created by the module of type MODULETYPE, where the collection has
-// instance name "instance", which is optional. For example, to create
-// a PtrMaker for an std::vector of A in an event evt, and current module,
-// we will use the PtrMaker as follows:
-// PtrMaker<A>make_Aptr(evt, *this);
+// Step I has two cases; the product and ptrs are constructed in the
+// same module, or in different modules. For each case, there is a way
+// to construct a PtrMaker object.
 //
-// case 2: In this case, the collection of type C is created in another
-// module. We need the product ID to create an object of PtrMaker. The
-// way to get a product ID is to first get an art::Handle and then use
-// "id()". Assuming, h is the art::Handle to the data product, and evt is
-// art::Event, then we will use it as follows:
-// art::Handle<std::vector<A>> h;
-// PtrMaker<A> make_Aptr(evt, h.id());
+// case 1: Construct a PtrMaker object that creates Ptrs into a
+// collection of type C, the most common one is std::vector and hence
+// is the default, created by the module of type Module, where the
+// collection has instance name "instance", which is optional. For
+// example, to create a PtrMaker for an std::vector<A> in an event
+// evt, and current module, we will use the PtrMaker as follows:
+//
+//   PtrMaker<A> make_Aptr{evt, *this}; // or
+//   auto make_Aptr = PtrMaker<A>::create<std::vector<A>>(evt, *this);
+//
+// If a container other std::vector<A> is desired, the static function
+// 'create' must be used instead of one of the constructors.
+//
+// case 2: In this case, the collection of type C is created in
+// another module. We need the product ID to create an object of
+// PtrMaker. The way to get a product ID is to first get an
+// art::Handle and then use "id()". Assuming, h is the art::Handle to
+// the data product, and evt is art::Event, then we will use it as
+// follows:
+//
+//   art::Handle<std::vector<A>> h;
+//   PtrMaker<A> make_Aptr{evt, h.id()};
 //
 // Step II: Use an index to create an art::Ptr to an object in the
 // slot indicated by "index"
-// auto const a = make_Aptr(index);
+//
+//   auto const a = make_Aptr(index);
 //
 ////////////////////////////////////////////////////////////////////////
 
@@ -46,52 +55,70 @@
 #include <vector>
 
 namespace art {
-  // to create art::Ptrs in to a particular collection in an event
-  template <class T>
+
+  // To create art::Ptrs into a particular collection in an event,
+  // subrun, run, or results.
+  template <typename T>
   class PtrMaker {
   public:
-    // Creates a PtrMaker that creates Ptrs in to a collection of type C created
-    // by the module of type MODULETYPE, where the collection has instance name
-    // "instance"
-    template <class EVENT, class MODULETYPE, class C = std::vector<T>>
-    PtrMaker(EVENT const& evt,
-             MODULETYPE const& module,
-             std::string const& instance = std::string());
+    // Creates a PtrMaker that creates Ptrs into a collection of type
+    // 'Container'.
+    template <typename Container, typename DataLevel, typename Module>
+    static PtrMaker<T> create(DataLevel const& E,
+                              Module const& module,
+                              std::string const& instance = {});
 
-    // use this constructor when making Ptrs to products created in other
-    // modules
-    template <class EVENT>
-    PtrMaker(EVENT const& evt, const art::ProductID& prodId);
+    // Creates a PtrMaker that creates Ptrs in to a collection of type
+    // std::vector<T> created by the module of type Module, where the
+    // collection has instance name "instance"
+    template <typename DataLevel, typename Module>
+    PtrMaker(DataLevel const& evt,
+             Module const& module,
+             std::string const& instance = {});
+
+    // Use this constructor when making Ptrs to products created in
+    // other modules
+    template <typename DataLevel>
+    PtrMaker(DataLevel const& evt, ProductID prodId);
 
     // Creates a Ptr to an object in the slot indicated by "index"
-    art::Ptr<T> operator()(std::size_t index) const;
+    Ptr<T> operator()(std::size_t index) const;
 
   private:
-    const art::ProductID prodId;
-    art::EDProductGetter const* prodGetter;
+    ProductID const prodId_;
+    EDProductGetter const* prodGetter_;
   };
 
-  template <class T>
-  template <class EVENT, class MODULETYPE, class C>
-  PtrMaker<T>::PtrMaker(EVENT const& evt,
-                        MODULETYPE const& module,
-                        std::string const& instance)
-    : prodId(module.template getProductID<C>(instance))
-    , prodGetter(evt.productGetter(prodId))
-  {}
-
-  template <class T>
-  template <class EVENT>
-  PtrMaker<T>::PtrMaker(EVENT const& evt, const art::ProductID& pid)
-    : prodId(pid), prodGetter(evt.productGetter(pid))
-  {}
-
-  template <class T>
-  art::Ptr<T>
-  PtrMaker<T>::operator()(size_t index) const
+  template <typename T>
+  template <typename Container, typename DataLevel, typename Module>
+  PtrMaker<T>
+  PtrMaker<T>::create(DataLevel const& evt,
+                      Module const& module,
+                      std::string const& instance)
   {
-    art::Ptr<T> artPtr(prodId, index, prodGetter);
-    return artPtr;
+    auto const pid = module.template getProductID<Container>(instance);
+    return PtrMaker<T>{evt, pid};
+  }
+
+  template <typename T>
+  template <typename DataLevel, typename Module>
+  PtrMaker<T>::PtrMaker(DataLevel const& evt,
+                        Module const& module,
+                        std::string const& instance)
+    : PtrMaker{evt, module.template getProductID<std::vector<T>>(instance)}
+  {}
+
+  template <typename T>
+  template <typename DataLevel>
+  PtrMaker<T>::PtrMaker(DataLevel const& evt, ProductID const pid)
+    : prodId_{pid}, prodGetter_{evt.productGetter(pid)}
+  {}
+
+  template <typename T>
+  Ptr<T>
+  PtrMaker<T>::operator()(size_t const index) const
+  {
+    return Ptr<T>{prodId_, index, prodGetter_};
   }
 }
 // end of namespace
--- art/test/Persistency/Common/testPtrMaker/PtrMakerProducer2_module.cc
+++ art/test/Persistency/Common/testPtrMaker/PtrMakerProducer2_module.cc
@@ -10,24 +10,22 @@
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
-#include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Principal/Run.h"
-#include "art/Framework/Principal/SubRun.h"
 #include "art/Persistency/Common/PtrMaker.h"
-#include "canvas/Utilities/InputTag.h"
-#include "fhiclcpp/ParameterSet.h"
-#include "messagefacility/MessageLogger/MessageLogger.h"
-#include <memory>
+#include "fhiclcpp/types/Atom.h"
 
-class PtrMakerProducer2;
+#include <cassert>
+#include <memory>
 
 class PtrMakerProducer2 : public art::EDProducer {
 public:
-  typedef std::vector<int> intvector_t;
-  typedef art::PtrVector<int> intPtrvector_t;
-  explicit PtrMakerProducer2(fhicl::ParameterSet const& p);
-  // The compiler-generated destructor is fine for non-base
-  // classes without bare pointers or other resource use.
+  using intvector_t = std::vector<int>;
+  using intPtrvector_t = art::PtrVector<int>;
+
+  struct Config {
+    fhicl::Atom<unsigned> nvalues{fhicl::Name{"nvalues"}};
+  };
+  using Parameters = art::EDProducer::Table<Config>;
+  explicit PtrMakerProducer2(Parameters const& p);
 
   // Plugins should not be copied or assigned.
   PtrMakerProducer2(PtrMakerProducer2 const&) = delete;
@@ -35,15 +33,13 @@ public:
   PtrMakerProducer2& operator=(PtrMakerProducer2 const&) = delete;
   PtrMakerProducer2& operator=(PtrMakerProducer2&&) = delete;
 
-  // Required functions.
-  void produce(art::Event& e) override;
-
 private:
-  int nvalues;
+  void produce(art::Event& e) override;
+  unsigned const nvalues_;
 };
 
-PtrMakerProducer2::PtrMakerProducer2(fhicl::ParameterSet const& p)
-  : nvalues(p.get<int>("nvalues"))
+PtrMakerProducer2::PtrMakerProducer2(Parameters const& p)
+  : nvalues_{p().nvalues()}
 {
   produces<intvector_t>();
   produces<intPtrvector_t>();
@@ -52,16 +48,26 @@ PtrMakerProducer2::PtrMakerProducer2(fhicl::ParameterSet const& p)
 void
 PtrMakerProducer2::produce(art::Event& e)
 {
-  std::cerr << "PtrMakerProducer 2 is running!\n";
-  int value_ = e.id().event();
-  std::unique_ptr<intvector_t> intvector(new intvector_t);
+  auto const value = e.id().event();
+  auto intvector = std::make_unique<intvector_t>();
   auto intptrs = std::make_unique<intPtrvector_t>();
-  art::PtrMaker<int> make_intptr(e, *this);
 
-  for (int i = 0; i != nvalues; ++i) {
-    intvector->push_back(value_ + i);
-    auto p = make_intptr(i);
-    intptrs->push_back(p);
+  // Make two PtrMakers--where one of them uses the named-constructor
+  // idiom ('create'), explicitly specifying the desired type.  The
+  // 'create' function is necessary if a collection other than
+  // std::vector is desired.
+  art::PtrMaker<int> const make_intptr1{e, *this};
+  auto const make_intptr2 =
+    art::PtrMaker<int>::create<std::vector<int>>(e, *this);
+
+  for (unsigned i{}; i != nvalues_; ++i) {
+    intvector->push_back(value + i);
+    auto p1 = make_intptr1(i);
+    auto p2 = make_intptr2(i);
+    // Ensure that the two art::Ptrs are equal, but only persist one
+    // copy.
+    assert(p1 == p2);
+    intptrs->push_back(p1);
   }
   e.put(std::move(intvector));
   e.put(std::move(intptrs));
