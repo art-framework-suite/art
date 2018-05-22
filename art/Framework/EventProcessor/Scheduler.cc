c00a56b986f15d568937f94c7c690980a8040060
Kyle Knoepfel <knoepfel@fnal.gov>
2018-05-22 09:41:31 -0500

    Provide nthread back-off based on OMP_NUM_THREADS.

--- art/Framework/EventProcessor/Scheduler.cc
+++ art/Framework/EventProcessor/Scheduler.cc
@@ -2,18 +2,49 @@
 // vim: set sw=2 expandtab :
 
 #include "art/Utilities/Globals.h"
+#include "cetlib/HorizontalRule.h"
+#include "cetlib/getenv.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "tbb/task.h"
 #include "tbb/task_arena.h"
 
+#include <cstdlib>
+#include <string>
+
 using fhicl::ParameterSet;
+
+namespace {
+  int
+  adjust_num_threads(int const specified_num_threads)
+  {
+    char const* p = std::getenv("OMP_NUM_THREADS");
+    if (p == nullptr) {
+      return specified_num_threads;
+    }
+
+    auto const max_threads = std::stoi(p);
+    if (specified_num_threads <= max_threads) {
+      return specified_num_threads;
+    }
+
+    cet::HorizontalRule const rule{80};
+    mf::LogAbsolute("MTconfig")
+      << rule('=') << '\n'
+      << "The specified number of threads (" << specified_num_threads
+      << ") exceeds the allowed number (" << max_threads << ").\n"
+      << "The allowed number of threads (" << max_threads
+      << ") will be used for this job.\n"
+      << rule('=');
+    return max_threads;
+  }
+}
 
 namespace art {
 
   Scheduler::Scheduler(Parameters const& ps)
     : actionTable_{ps().actionTable()}
-    , nThreads_{ps().num_threads()}
+    , nThreads_{adjust_num_threads(ps().num_threads())}
     , nSchedules_{ps().num_schedules()}
     , stackSize_{ps().stack_size()}
     , handleEmptyRuns_{ps().handleEmptyRuns()}
@@ -32,8 +63,9 @@ namespace art {
   {
     tbbManager_.initialize(nThreads_, stackSize_);
     mf::LogInfo("MTdiagnostics")
-      << "TBB has been configured to use a maximum of "
-      << tbb::this_task_arena::max_concurrency() << " threads,\n"
-      << "with a stack size of " << stackSize_ << " bytes.";
+      << "TBB has been configured to use:\n"
+      << "  - a maximum of " << tbb::this_task_arena::max_concurrency()
+      << " threads\n"
+      << "  - a stack size of " << stackSize_ << " bytes";
   }
 }
