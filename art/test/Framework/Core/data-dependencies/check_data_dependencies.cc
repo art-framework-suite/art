a7ea6080f91b626f5032571726fa34d12fd072b4
Kyle Knoepfel <knoepfel@fnal.gov>
2018-05-25 11:21:38 -0500

    Make dependency-checker tester more flexible.

--- art/test/Framework/Core/data-dependencies/check_data_dependencies.cc
+++ art/test/Framework/Core/data-dependencies/check_data_dependencies.cc
@@ -5,45 +5,73 @@
 #include "canvas/Utilities/Exception.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "fhiclcpp/make_ParameterSet.h"
+#include "fhiclcpp/types/OptionalAtom.h"
 #include "fhiclcpp/types/OptionalDelegatedParameter.h"
 #include "fhiclcpp/types/OptionalSequence.h"
 #include "fhiclcpp/types/OptionalTable.h"
 #include "fhiclcpp/types/Table.h"
+#include "fhiclcpp/types/TupleAs.h"
 
 #include <cassert>
 #include <fstream>
 #include <iostream>
 
 using namespace art::detail;
+using namespace fhicl;
 using namespace std::string_literals;
+using std::string;
 
 namespace {
+  // For producing products
+  struct TypeAndInstance {
+    string friendlyClassName;
+    string productInstanceName;
+  };
+
+  // For consuming products
+  struct TypeAndTag {
+    string friendlyClassName;
+    art::InputTag inputTag;
+  };
+
   struct TopLevelTable {
-    struct Source {
+    struct TestProperties {
+      Atom<bool> graph_failure_expected{Name{"graph_failure_expected"}};
+      OptionalAtom<string> error_message{Name{"error_message"}};
     };
-    fhicl::OptionalTable<Source> source{fhicl::Name{"source"}};
-    fhicl::OptionalDelegatedParameter physics{fhicl::Name{"physics"}};
-    fhicl::OptionalDelegatedParameter outputs{fhicl::Name{"outputs"}};
+    Table<TestProperties> test_properties{Name{"test_properties"}};
+    Atom<string> process_name{Name{"process_name"}};
+    struct Source {
+      Atom<string> module_type{Name{"module_type"}};
+    };
+    OptionalTable<Source> source{Name{"source"}};
+    OptionalDelegatedParameter physics{Name{"physics"}};
+    OptionalDelegatedParameter outputs{Name{"outputs"}};
   };
 
   struct ModifierModuleConfig {
-    fhicl::OptionalSequence<std::string> depends_on{fhicl::Name{"depends_on"}};
+    OptionalSequence<TupleAs<TypeAndInstance(string, string)>> produces{
+      Name{"produces"}};
+    OptionalSequence<TupleAs<TypeAndTag(string, art::InputTag)>> consumes{
+      Name{"consumes"}};
+    OptionalSequence<string> consumesMany{Name{"consumesMany"}};
   };
 
   struct ObserverModuleConfig {
-    fhicl::OptionalSequence<std::string> depends_on{fhicl::Name{"depends_on"}};
-    fhicl::OptionalSequence<std::string> select_events{
-      fhicl::Name{"SelectEvents"}};
+    OptionalSequence<TupleAs<TypeAndTag(string, art::InputTag)>> consumes{
+      Name{"consumes"}};
+    OptionalSequence<string> consumesMany{Name{"consumesMany"}};
+    OptionalSequence<string> select_events{Name{"SelectEvents"}};
   };
 
   paths_to_modules_t
-  get_paths_to_modules(fhicl::ParameterSet const& physics)
+  get_paths_to_modules(ParameterSet const& physics)
   {
     paths_to_modules_t result;
     for (auto const& name : physics.get_names()) {
       if (!physics.is_key_to_sequence(name))
         continue;
-      auto const tmp = physics.get<std::vector<std::string>>(name);
+      auto const tmp = physics.get<std::vector<string>>(name);
       configs_t configs;
       cet::transform_all(tmp, back_inserter(configs), [](auto const& str) {
         return art::WorkerInPath::ConfigInfo{str, art::WorkerInPath::Normal};
@@ -57,20 +85,19 @@ namespace {
   names_t const tables_with_observers{"physics.analyzers", "outputs"};
 
   inline bool
-  module_found_in_table(std::string const& module_name,
-                        fhicl::ParameterSet const& pset,
-                        std::string const& table_name)
+  module_found_in_table(string const& module_name,
+                        ParameterSet const& pset,
+                        string const& table_name)
   {
     if (!pset.has_key(table_name)) {
       return false;
     }
-    auto const& table = pset.get<fhicl::ParameterSet>(table_name);
+    auto const& table = pset.get<ParameterSet>(table_name);
     return table.has_key(module_name);
   }
 
   inline art::ModuleType
-  module_found_with_type(std::string const& module_name,
-                         fhicl::ParameterSet const& pset)
+  module_found_with_type(string const& module_name, ParameterSet const& pset)
   {
     if (module_found_in_table(module_name, pset, "physics.producers"))
       return art::ModuleType::producer;
@@ -83,7 +110,7 @@ namespace {
     return art::ModuleType::non_art;
   }
 
-  inline std::string
+  inline string
   table_for_module_type(art::ModuleType const module_type)
   {
     if (module_type == art::ModuleType::producer)
@@ -98,8 +125,8 @@ namespace {
   }
 
   bool
-  module_found_in_tables(std::string const& module_name,
-                         fhicl::ParameterSet const& pset,
+  module_found_in_tables(string const& module_name,
+                         ParameterSet const& pset,
                          names_t const& table_names)
   {
     for (auto const& table_name : table_names) {
@@ -111,12 +138,12 @@ namespace {
   }
 
   paths_to_modules_t
-  select_paths(fhicl::ParameterSet const& pset,
+  select_paths(ParameterSet const& pset,
                names_t const& tables,
                paths_to_modules_t& paths_to_modules)
   {
     paths_to_modules_t result;
-    std::vector<std::string> paths_to_erase;
+    std::vector<string> paths_to_erase;
     for (auto const& pr : paths_to_modules) {
       auto const& path_name = pr.first;
       auto const& modules = pr.second;
@@ -168,6 +195,52 @@ namespace {
     }
     return result;
   }
+
+  template <typename T>
+  std::set<art::ProductInfo>
+  sorted_dependencies(Table<T> const& module)
+  {
+    std::set<art::ProductInfo> sorted_deps;
+    std::vector<TypeAndTag> deps;
+    if (module().consumes(deps)) {
+      for (auto const& dep : deps) {
+        sorted_deps.emplace(art::ProductInfo::ConsumableType::Product,
+                            dep.friendlyClassName,
+                            dep.inputTag.label(),
+                            dep.inputTag.instance(),
+                            art::ProcessTag{dep.inputTag.process(), "test"});
+      }
+    }
+    return sorted_deps;
+  }
+
+  void
+  fill_module_info(ParameterSet const& pset,
+                   string const& path_name,
+                   configs_t const& module_configs,
+                   art::detail::collection_map_t& modules)
+  {
+    for (auto const& config : module_configs) {
+      auto const& module_name = config.label;
+      auto& info = modules[module_name];
+      info.paths.insert(path_name);
+      info.module_type = module_found_with_type(module_name, pset);
+      auto const table_name = table_for_module_type(info.module_type);
+      auto const& table =
+        pset.get<ParameterSet>(table_name + "." + module_name);
+      if (is_modifier(info.module_type)) {
+        Table<ModifierModuleConfig> const mod{table};
+        info.product_dependencies = sorted_dependencies(mod);
+      } else {
+        Table<ObserverModuleConfig> const mod{table};
+        info.product_dependencies = sorted_dependencies(mod);
+        std::vector<string> sel;
+        if (mod().select_events(sel)) {
+          info.select_events = std::set<string>(cbegin(sel), cend(sel));
+        }
+      }
+    }
+  }
 }
 
 int
@@ -175,17 +248,20 @@ main(int argc, char** argv) try {
   if (argc != 2)
     return 1;
 
-  std::string const filename{argv[1]};
+  string const filename{argv[1]};
 
-  fhicl::ParameterSet pset;
+  ParameterSet pset;
   cet::filepath_maker maker{};
   make_ParameterSet(filename, maker, pset);
-  fhicl::Table<TopLevelTable> table{pset};
+  Table<TopLevelTable> table{pset};
+  auto const& test_properties = table().test_properties();
 
-  fhicl::ParameterSet physics;
+  ParameterSet physics;
   if (!table().physics.get_if_present(physics)) {
     return 0;
   }
+
+  // Form the paths
   auto paths_to_modules = get_paths_to_modules(physics);
   auto const trigger_paths =
     select_paths(pset, tables_with_modifiers, paths_to_modules);
@@ -203,91 +279,70 @@ main(int argc, char** argv) try {
     source_info.paths = {"end_path"};
   }
 
-  auto fill_module_info = [&modules](fhicl::ParameterSet const& pset,
-                                     std::string const& path_name,
-                                     configs_t const& module_configs) {
-    for (auto const& config : module_configs) {
-      auto const& module_name = config.label;
-      auto& info = modules[module_name];
-      info.paths.insert(path_name);
-      info.module_type = module_found_with_type(module_name, pset);
-      if (is_modifier(info.module_type)) {
-        auto const table_name = table_for_module_type(info.module_type);
-        auto const& table =
-          pset.get<fhicl::ParameterSet>(table_name + "." + module_name);
-        fhicl::Table<ModifierModuleConfig> mod{table};
-        std::vector<std::string> deps;
-        if (mod().depends_on(deps)) {
-          // Assume all the same type for simplicity...for now
-          std::set<art::ProductInfo> sorted_deps;
-          for (auto const& module_label : deps) {
-            sorted_deps.emplace(art::ProductInfo::ConsumableType::Product,
-                                art::TypeID{typeid(int)},
-                                module_label,
-                                ""s,
-                                art::ProcessTag{});
-          }
-          info.product_dependencies = move(sorted_deps);
-        }
-      } else {
-        auto const table_name = table_for_module_type(info.module_type);
-        auto const& table =
-          pset.get<fhicl::ParameterSet>(table_name + "." + module_name);
-        fhicl::Table<ObserverModuleConfig> mod{table};
-        std::vector<std::string> deps;
-        if (mod().depends_on(deps)) {
-          // Assume all the same type for simplicity...for now
-          std::set<art::ProductInfo> sorted_deps;
-          for (auto const& module_label : deps) {
-            sorted_deps.emplace(art::ProductInfo::ConsumableType::Product,
-                                art::TypeID{typeid(int)},
-                                module_label,
-                                ""s,
-                                art::ProcessTag{});
-          }
-          info.product_dependencies = move(sorted_deps);
-        }
-        std::vector<std::string> sel;
-        if (mod().select_events(sel)) {
-          info.select_events = std::set<std::string>(cbegin(sel), cend(sel));
-        }
-      }
-    }
-  };
-
   for (auto const& path : trigger_paths) {
-    fill_module_info(pset, path.first, path.second);
+    fill_module_info(pset, path.first, path.second, modules);
   }
 
   if (!trigger_paths.empty()) {
     modules["TriggerResults"] = ModuleGraphInfo{art::ModuleType::producer};
   }
 
-  fill_module_info(pset, "end_path", end_path);
+  fill_module_info(pset, "end_path", end_path, modules);
 
+  // Build the graph
   ModuleGraphInfoMap const modInfos{modules};
   auto const module_graph =
     art::detail::make_module_graph(modInfos, trigger_paths, end_path);
   auto const& err = module_graph.second;
-  int rc{};
+  bool graph_failure{false};
+  std::ostringstream oss;
   if (!err.empty()) {
-    std::cout << err << '\n';
-    rc = 1;
+    oss << err << '\n';
+    graph_failure = true;
   }
 
   auto const pos = filename.find(".fcl");
-  std::string const basename =
-    (pos != std::string::npos) ? filename.substr(0, pos) : filename;
+  string const basename =
+    (pos != string::npos) ? filename.substr(0, pos) : filename;
   std::ofstream ofs{basename + ".dot"};
   print_module_graph(ofs, modInfos, module_graph.first);
+
+  // Check if test properties have been satisfied
+  int rc{};
+  bool const graph_failure_expected = test_properties.graph_failure_expected();
+  if (graph_failure != graph_failure_expected) {
+    std::cerr << " Graph failure value is not what is expected:\n"
+              << "   should be " << std::boolalpha << graph_failure_expected
+              << '\n'
+              << "   but it is " << graph_failure << '\n';
+    rc = 1;
+  }
+  std::string expected_msg;
+  if (test_properties.error_message(expected_msg)) {
+    auto const msg = oss.str();
+    std::regex const re{expected_msg};
+    if (!graph_failure) {
+      std::cerr << " An error message was generated even though there was no "
+                   "graph failure.\n";
+      rc = 2;
+    } else if (!std::regex_search(msg, re)) {
+      std::cerr << " The error message does not match what was expected:\n"
+                << "   Actual: " << msg << '\n'
+                << "   Expected: " << expected_msg << '\n';
+      rc = 3;
+    }
+  }
   return rc;
 }
-catch (fhicl::detail::validationException const& v) {
+catch (detail::validationException const& v) {
   std::cerr << v.what();
+  return 1;
 }
 catch (std::exception const& e) {
   std::cerr << e.what() << '\n';
+  return 1;
 }
 catch (...) {
   std::cerr << "Job failed.\n";
+  return 1;
 }
