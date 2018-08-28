db621a4179a6111e27429e98645e5c3029905409
Kyle Knoepfel <knoepfel@fnal.gov>
2018-08-28 14:09:39 -0500

    Move integration tests to critic.

--- art/test/Integration/IntTestAnalyzer_module.cc
+++
@@ -1,9 +0,0 @@
-#include "art/Framework/Core/ModuleMacros.h"
-#include "art/test/Integration/GenericOneSimpleProductAnalyzer.h"
-#include "art/test/TestObjects/ToyProducts.h"
-
-namespace arttest {
-  using IntTestAnalyzer = GenericOneSimpleProductAnalyzer<int, IntProduct>;
-}
-
-DEFINE_ART_MODULE(arttest::IntTestAnalyzer)
