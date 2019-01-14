d3ba0914cf290be47966380eec73dbfc0306c688
Kyle Knoepfel <knoepfel@fnal.gov>
2019-01-14 13:33:01 -0600

    Remove now unnecessary Boost_SYSTEM_LIBRARY dependency.

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -47,8 +47,7 @@ function(simple_plugin name type)
       fhiclcpp
       cetlib
       cetlib_except
-      ${Boost_FILESYSTEM_LIBRARY}
-      ${Boost_SYSTEM_LIBRARY})
+      ${Boost_FILESYSTEM_LIBRARY})
   elseif("${type}" STREQUAL "module" OR "${type}" STREQUAL "source")
     list(INSERT simple_plugin_liblist 0
       art_Framework_Core
@@ -60,22 +59,19 @@ function(simple_plugin name type)
       fhiclcpp
       cetlib
       cetlib_except
-      ${Boost_FILESYSTEM_LIBRARY}
-      ${Boost_SYSTEM_LIBRARY})
+      ${Boost_FILESYSTEM_LIBRARY})
   elseif("${type}" STREQUAL "tool")
     list(INSERT simple_plugin_liblist 0
       art_Utilities
       fhiclcpp
       cetlib
       cetlib_except
-      ${Boost_FILESYSTEM_LIBRARY}
-      ${Boost_SYSTEM_LIBRARY})
+      ${Boost_FILESYSTEM_LIBRARY})
   endif()
   if ("${type}" STREQUAL "source")
     list(INSERT simple_plugin_liblist 0
       art_Framework_IO_Sources
-      ${Boost_FILESYSTEM_LIBRARY}
-      ${Boost_SYSTEM_LIBRARY})
+      ${Boost_FILESYSTEM_LIBRARY})
   endif()
   check_ups_version(cetbuildtools ${cetbuildtools_UPS_VERSION} v4_05_00 PRODUCT_MATCHES_VAR BP_HAS_SOURCE)
   if(SP_SOURCE)
