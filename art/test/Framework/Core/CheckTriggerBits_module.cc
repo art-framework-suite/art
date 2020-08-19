8904b7ab1fa4908f4fdae604898dd6848e470d52
Kyle Knoepfel <knoepfel@fnal.gov>
2020-08-19 11:07:10 -0500

    Reproduce bug #24821: trigger bit assignment still incorrect.

--- art/test/Framework/Core/CheckTriggerBits_module.cc
+++ art/test/Framework/Core/CheckTriggerBits_module.cc
@@ -1,10 +1,10 @@
 #include "cetlib/quiet_unit_test.hpp"
 
-#include "art/Framework/Core/SharedAnalyzer.h"
-#include "art/Framework/Services/System/TriggerNamesService.h"
 #include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/SharedAnalyzer.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/System/TriggerNamesService.h"
 #include "canvas/Utilities/InputTag.h"
 
 namespace {
@@ -14,19 +14,21 @@ namespace {
       fhicl::Sequence<std::string> paths{fhicl::Name{"paths"}};
     };
     using Parameters = Table<Config>;
-    explicit CheckTriggerBits(Parameters const& p,
-                              art::ProcessingFrame const&);
+    explicit CheckTriggerBits(Parameters const& p, art::ProcessingFrame const&);
 
   private:
-    void analyze(art::Event const&,
-                 art::ProcessingFrame const&) override;
+    void analyze(art::Event const&, art::ProcessingFrame const&) override;
     std::vector<std::string> const paths_;
+    std::vector<bool> const path_results_;
+    art::ProductToken<art::TriggerResults> const token_;
   };
 
   CheckTriggerBits::CheckTriggerBits(Parameters const& p,
                                      art::ProcessingFrame const&)
     : SharedAnalyzer{p}
     , paths_{p().paths()}
+    , path_results_{true, false}
+    , token_{consumes<art::TriggerResults>("TriggerResults")}
   {
     async<art::InEvent>();
 
@@ -40,9 +42,13 @@ namespace {
   }
 
   void
-  CheckTriggerBits::analyze(art::Event const&,
-                            art::ProcessingFrame const&)
-  {}
+  CheckTriggerBits::analyze(art::Event const& e, art::ProcessingFrame const&)
+  {
+    auto const& trigger_results = *e.getValidHandle(token_);
+    for (std::size_t i = 0; i != trigger_results.size(); ++i) {
+      BOOST_TEST(path_results_[i] == trigger_results.accept(i));
+    }
+  }
 }
 
 DEFINE_ART_MODULE(CheckTriggerBits)
