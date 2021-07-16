3e4a08b0e74ff2c3cf595034791264a4c8bbf4a1
Kyle Knoepfel <knoepfel@fnal.gov>
2021-07-16 12:33:39 -0500

    Fix bug #26049: reconstitutes adjusted for multiple emulated source names.

--- art/test/Framework/Core/ProductRegistryHelper_t.cc
+++ art/test/Framework/Core/ProductRegistryHelper_t.cc
@@ -2,7 +2,10 @@
 #include "cetlib/quiet_unit_test.hpp"
 
 #include "art/Framework/Core/ProductRegistryHelper.h"
+#include "art/Persistency/Provenance/ModuleDescription.h"
+#include "canvas/Persistency/Common/Assns.h"
 #include "canvas/Utilities/Exception.h"
+#include "fhiclcpp/ParameterSet.h"
 
 using namespace art;
 
@@ -19,6 +22,14 @@ namespace {
       return e.explain_self().find(msg_) != std::string::npos;
     }
   };
+
+  auto const dummy_pset_id = fhicl::ParameterSet{}.id();
+  ModuleDescription const md{dummy_pset_id,
+                             "_NAMEERROR_",
+                             "_LABELERROR_",
+                             ModuleThreadingType::legacy,
+                             ProcessConfiguration{"Process", dummy_pset_id, {}},
+                             true /*isEmulated*/};
 }
 
 BOOST_AUTO_TEST_SUITE(ProductRegistryHelper_t)
@@ -28,7 +39,7 @@ BOOST_AUTO_TEST_CASE(produces)
   ProductRegistryHelper prh{product_creation_mode::produces};
   prh.produces<int>();
   BOOST_CHECK_EXCEPTION((prh.reconstitutes<int, InEvent>("label", "instance")),
-                        art::Exception,
+                        Exception,
                         matches{"'produces' should have been called instead"});
 }
 
@@ -39,6 +50,25 @@ BOOST_AUTO_TEST_CASE(reconstitutes)
   BOOST_CHECK_EXCEPTION(prh.produces<int>(),
                         art::Exception,
                         matches{"'reconstitutes' should have been called instead"});
+}
+
+BOOST_AUTO_TEST_CASE(reconstitutes_different_module_labels)
+{
+  ProductRegistryHelper prh{product_creation_mode::reconstitutes};
+  prh.reconstitutes<int, InEvent>("label1");
+  prh.reconstitutes<int, InEvent>("label2");
+  BOOST_CHECK_NO_THROW(prh.fillDescriptions(md));
+}
+
+BOOST_AUTO_TEST_CASE(reconstitutes_assns_reversed_types)
+{
+  ProductRegistryHelper prh{product_creation_mode::reconstitutes};
+  prh.reconstitutes<Assns<int, double>, InEvent>("label");
+  prh.reconstitutes<Assns<double, int>, InEvent>("label");
+  BOOST_CHECK_EXCEPTION(
+    prh.fillDescriptions(md),
+    Exception,
+    matches{"That friendly name has already been registered for this module."});
 }
 
 BOOST_AUTO_TEST_SUITE_END()
