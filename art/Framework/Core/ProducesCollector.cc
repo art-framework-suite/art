d3128a80230734bacfc73b4a9878bc8935a99807
Kyle Knoepfel <knoepfel@fnal.gov>
2021-07-16 12:33:39 -0500

    Fix bug #26049: reconstitutes adjusted for multiple emulated source names.

--- art/Framework/Core/ProducesCollector.cc
+++ art/Framework/Core/ProducesCollector.cc
@@ -2,6 +2,8 @@
 #include "art/Persistency/Provenance/ModuleDescription.h"
 #include "canvas/Persistency/Provenance/BranchDescription.h"
 
+#include "range/v3/view/map.hpp"
+
 #include <map>
 #include <set>
 #include <string>
@@ -24,25 +26,37 @@ namespace art {
     // friendlyClassName, which resolves to the same string for
     // Assns<A,B> and Assns<B,A>.
     void
-    check_for_duplicate_Assns(TypeLabelLookup_t const& typeLabels)
+    check_for_duplicate_friendly_names(TypeLabelLookup_t const& typeLabels)
     {
+      using ranges::views::keys;
       map<string, set<string>> instanceToFriendlyNames;
-      for (auto const& pr : typeLabels) {
-        auto const& tl = pr.first;
-        auto result = instanceToFriendlyNames[tl.productInstanceName()].emplace(
-          tl.typeID().friendlyClassName());
+      for (auto const& type_label : typeLabels | keys) {
+        auto unique_entry = type_label.productInstanceName();
+        if (type_label.hasEmulatedModule()) {
+          // Whenever 'produces' is called, the module label is always
+          // the same for a given ProducesCollector object.  However,
+          // for 'reconstitutes', the user provides a module label,
+          // and it is therefore necessary for this check to allow
+          // different module labels with the same type and instance
+          // name.
+          unique_entry += "_" + type_label.emulatedModule();
+        }
+        auto result = instanceToFriendlyNames[unique_entry].emplace(
+          type_label.typeID().friendlyClassName());
         if (!result.second) {
-          throw Exception(errors::LogicError, "check_for_duplicate_Assns: ")
+          throw Exception(errors::LogicError,
+                          "check_for_duplicate_friendly_names: ")
             << "An attempt has been made to call the equivalent of\n\n"
-            << "   produces<" << tl.typeID().className() << ">(\""
-            << tl.productInstanceName() << "\")\n\n"
+            << "   produces<" << type_label.typeID().className() << ">(\""
+            << type_label.productInstanceName() << "\")\n\n"
             << "which results in a prepared (\"friendly\") name of:\n\n"
             << "   " << *result.first << "\n\n"
             << "That friendly name has already been registered for this "
                "module.\n"
             << "Please check to make sure that produces<> has not already "
                "been\n"
-            << "called for an Assns<> with reversed template arguments.  Such\n"
+            << "called for an Assns<> with reversed template arguments.  "
+               "Such\n"
             << "behavior is not supported.  Contact artists@fnal.gov for "
                "guidance.\n";
         }
@@ -76,7 +90,7 @@ namespace art {
     auto fillDescriptionsPerBT = [this, &md](BranchType const bt) {
       auto& expectedProducts = typeLabelList_[bt];
       // Go through products that will be produced in the current process.
-      check_for_duplicate_Assns(expectedProducts);
+      check_for_duplicate_friendly_names(expectedProducts);
 
       for (auto& [typeLabel, pd] : expectedProducts) {
         pd = BranchDescription{bt,
