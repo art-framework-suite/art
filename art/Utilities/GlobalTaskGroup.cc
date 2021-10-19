367881770cdc7a8712be6111c8378addf4293211
Kyle Knoepfel <knoepfel@fnal.gov>
2021-10-19 11:43:11 -0500

    Resolve bug #26339: respect exception thrown in path.

--- art/Utilities/GlobalTaskGroup.cc
+++ art/Utilities/GlobalTaskGroup.cc
@@ -10,10 +10,14 @@ void
 art::GlobalTaskGroup::may_run(hep::concurrency::WaitingTaskPtr task,
                               std::exception_ptr ex_ptr)
 {
+  // A non-null exception pointer is always registered with the task
+  // even if the task's 'done' count will not yet decrement to zero.
+  // This ensures that once the task's 'done' count is zero, an
+  // exception will be registered with the task when it is launched.
+  if (ex_ptr) {
+    task->dependentTaskFailed(ex_ptr);
+  }
   if (task->decrement_done_count() == 0u) {
-    group_.run([t = std::move(task), ex_ptr] {
-      t->dependentTaskFailed(ex_ptr);
-      (*t)();
-    });
+    group_.run([t = std::move(task)] { (*t)(); });
   }
 }
