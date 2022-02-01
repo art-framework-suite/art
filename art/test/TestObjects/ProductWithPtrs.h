b9fa9dd1846702ec4656e103b119bbc284e713c6
Kyle Knoepfel <knoepfel@fnal.gov>
2022-02-01 10:34:02 -0600

    Remove unused preprocessor selection.

--- art/test/TestObjects/ProductWithPtrs.h
+++ art/test/TestObjects/ProductWithPtrs.h
@@ -13,62 +13,35 @@ namespace arttest {
 
 class arttest::ProductWithPtrs {
 public:
-  ProductWithPtrs();
+  ProductWithPtrs() = default;
 
-  ProductWithPtrs(
-#ifndef ART_NO_MIX_PTRVECTOR
-    art::PtrVector<double> const& pvd,
-#endif
-    std::vector<art::Ptr<double>> const& vpd);
+  ProductWithPtrs(art::PtrVector<double> const& pvd,
+                  std::vector<art::Ptr<double>> const& vpd);
 
   // Observers
-#ifndef ART_NO_MIX_PTRVECTOR
   art::PtrVector<double> const& ptrVectorDouble() const;
-#endif
   std::vector<art::Ptr<double>> const& vectorPtrDouble() const;
 
   // Modifiers
-#ifndef ART_NO_MIX_PTRVECTOR
   art::PtrVector<double>& ptrVectorDouble();
-#endif
   std::vector<art::Ptr<double>>& vectorPtrDouble();
 
   // Public to allow tests.
-#ifndef ART_NO_MIX_PTRVECTOR
-  art::PtrVector<double> pvd_;
-#endif
-  std::vector<art::Ptr<double>> vpd_;
+  art::PtrVector<double> pvd_{};
+  std::vector<art::Ptr<double>> vpd_{};
 };
 
-inline arttest::ProductWithPtrs::ProductWithPtrs()
-  :
-#ifndef ART_NO_MIX_PTRVECTOR
-  pvd_()
-  ,
-#endif
-  vpd_()
-{}
-
 inline arttest::ProductWithPtrs::ProductWithPtrs(
-#ifndef ART_NO_MIX_PTRVECTOR
   art::PtrVector<double> const& pvd,
-#endif
   std::vector<art::Ptr<double>> const& vpd)
-  :
-#ifndef ART_NO_MIX_PTRVECTOR
-  pvd_(pvd)
-  ,
-#endif
-  vpd_(vpd)
+  : pvd_(pvd), vpd_(vpd)
 {}
 
-#ifndef ART_NO_MIX_PTRVECTOR
 inline art::PtrVector<double> const&
 arttest::ProductWithPtrs::ptrVectorDouble() const
 {
   return pvd_;
 }
-#endif
 
 inline std::vector<art::Ptr<double>> const&
 arttest::ProductWithPtrs::vectorPtrDouble() const
@@ -76,13 +49,11 @@ arttest::ProductWithPtrs::vectorPtrDouble() const
   return vpd_;
 }
 
-#ifndef ART_NO_MIX_PTRVECTOR
 inline art::PtrVector<double>&
 arttest::ProductWithPtrs::ptrVectorDouble()
 {
   return pvd_;
 }
-#endif
 
 inline std::vector<art::Ptr<double>>&
 arttest::ProductWithPtrs::vectorPtrDouble()
