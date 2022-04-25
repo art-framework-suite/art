6775e4fed9964902ebc73aee4fd01934ba004a5e
Chris Green <greenc@fnal.gov>
2022-04-25 13:43:08 -0500

    Avoid unwanted dependencies when building and using tools

--- Modules/art::tool.cmake
+++ Modules/art::tool.cmake
@@ -11,6 +11,13 @@ include(BasicPlugin)
 macro(art::tool NAME)
   if (TARGET art_plugin_types::tool)
     set(_art_tool_deps LIBRARIES REG art_plugin_types::tool)
+    if (TARGET art_plugin_types::tool_interface)
+      list(APPEND _art_tool_deps
+        CONDITIONAL art_plugin_types::tool_interface)
+    else()
+      list(APPEND _art_tool_deps
+        CONDITIONAL fhiclcpp::types fhiclcpp::fhiclcpp)
+    endif()
   else()
     # Older art suites.
     set(_art_tool_deps LIBRARIES CONDITIONAL
