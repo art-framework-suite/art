21467131387b43b9b09aee3062ce124650ef0352
Zachary Evans <ztevans@fnal.gov>
2024-03-01 16:54:02 -0600

    replace metaprogramming in `ServiceHandleAllowed.h` with concepts

--- art/Framework/Services/Registry/detail/ServiceHandleAllowed.h
+++ art/Framework/Services/Registry/detail/ServiceHandleAllowed.h
@@ -10,21 +10,24 @@
 // definition, then the 'handle_allowed_v<T>' value is 'true'.
 // ======================================================================
 
-#include <type_traits>
 #include <concepts>
+#include <type_traits>
 
 namespace art::detail {
-  template typename T
-  concept handle_allowed = T::service_handle_allowed == true;
-  // template <typename T, typename = void>
-  // struct handle_allowed : std::true_type {};
-  //
-  // template <typename T>
-  // struct handle_allowed<T, std::enable_if_t<!T::service_handle_allowed>>
-  //   : std::false_type {};
-  //
-  // template <typename T>
-  // bool constexpr handle_allowed_v{handle_allowed<T>::value};
+  template <typename T>
+  concept handle_not_allowed = requires {
+                                 {
+                                   T::service_handle_allowed
+                                   } -> std::same_as<bool>;
+                               } && !
+  T::service_handle_allowed;
+
+  template <typename T>
+  concept handle_allowed = !
+  handle_not_allowed<T>;
+
+  template <typename T>
+  concept handle_allowed_v = handle_allowed<T>;
 }
 
 #endif /* art_Framework_Services_Registry_detail_ServiceHandleAllowed_h */
